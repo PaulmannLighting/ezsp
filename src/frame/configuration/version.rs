609bc4d1e8266661add56c2a357f41c006f3370c
Richard Neumann <neumann@paulmann.de>
2023-10-09 13:28:30 +0200

    Implement Command and Response

--- src/frame/configuration/version.rs
+++ src/frame/configuration/version.rs
@@ -5,9 +5,34 @@ pub struct Command {
     desired_protocol_version: u8,
 }
 
+impl Command {
+    pub const fn new(header: LegacyHeader, desired_protocol_version: u8) -> Self {
+        Self {
+            header,
+            desired_protocol_version,
+        }
+    }
+}
+
 pub struct Response {
     header: LegacyHeader,
     protocol_version: u8,
     stack_type: u8,
     stack_version: u8,
+}
+
+impl Response {
+    pub const fn new(
+        header: LegacyHeader,
+        protocol_version: u8,
+        stack_type: u8,
+        stack_version: u8,
+    ) -> Self {
+        Self {
+            header,
+            protocol_version,
+            stack_type,
+            stack_version,
+        }
+    }
 }

44497d0091840ce0529019ba9dd7389546074022
Richard Neumann <neumann@paulmann.de>
2023-10-09 13:26:03 +0200

    Reduce hex numbers to 2 digits

--- src/dongle/stack/policy.rs
+++ src/dongle/stack/policy.rs
@@ -2,14 +2,14 @@ use num_derive::{FromPrimitive, ToPrimitive};
 
 #[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd, FromPrimitive, ToPrimitive)]
 pub enum Policy {
-    TrustCenter = 0x0000,
-    BindingModification = 0x0001,
-    UnicastReplies = 0x0002,
-    PollHandler = 0x0003,
-    MessageContentsInCallback = 0x0004,
-    TcKeyRequest = 0x0005,
-    KeyRequest = 0x0006,
-    PacketValidateLibrary = 0x0007,
-    Zll = 0x0008,
-    TcJoinsUsingWellKnownKey = 0x0009,
+    TrustCenter = 0x00,
+    BindingModification = 0x01,
+    UnicastReplies = 0x02,
+    PollHandler = 0x03,
+    MessageContentsInCallback = 0x04,
+    TcKeyRequest = 0x05,
+    KeyRequest = 0x06,
+    PacketValidateLibrary = 0x07,
+    Zll = 0x08,
+    TcJoinsUsingWellKnownKey = 0x09,
 }

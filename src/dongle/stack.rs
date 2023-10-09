32eb736de01235e7ff9ef2f3b2fe67375cdc2d7e
Richard Neumann <neumann@paulmann.de>
2023-10-09 13:25:08 +0200

    Implement Stack config struct

--- src/dongle/stack.rs
+++ src/dongle/stack.rs
@@ -12,3 +12,17 @@ pub struct Stack {
     config: HashMap<Config, u8>,
     policy: HashMap<Policy, Decision>,
 }
+
+impl Stack {
+    pub fn new(config: HashMap<Config, u8>, policy: HashMap<Policy, Decision>) -> Self {
+        Self { config, policy }
+    }
+
+    pub fn config(&self) -> &HashMap<Config, u8> {
+        &self.config
+    }
+
+    pub fn policy(&self) -> &HashMap<Policy, Decision> {
+        &self.policy
+    }
+}

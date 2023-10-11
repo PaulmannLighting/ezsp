42f530aecbe2448f067c8bc19ed89630db4a675a
Richard Neumann <neumann@paulmann.de>
2023-10-11 15:46:42 +0200

    Implement setConfigurationValue()

--- src/frame/configuration.rs
+++ src/frame/configuration.rs
@@ -1,2 +1,3 @@
 mod get_configuration_value;
+mod set_configuration_value;
 mod version;
---
+++ src/frame/configuration/set_configuration_value.rs
@@ -0,0 +1,59 @@
+use crate::config;
+use crate::frame::header::Header;
+use crate::frame::Frame;
+use crate::status::Status;
+
+const ID: u16 = 0x0053;
+
+#[derive(Debug)]
+pub struct Command {
+    header: Header,
+    config_id: config::Id,
+    value: u16,
+}
+
+impl Command {
+    pub const fn new(header: Header, config_id: config::Id, value: u16) -> Self {
+        Self {
+            header,
+            config_id,
+            value,
+        }
+    }
+
+    pub const fn config_id(&self) -> &config::Id {
+        &self.config_id
+    }
+
+    pub const fn value(&self) -> u16 {
+        self.value
+    }
+}
+
+impl Frame<ID> for Command {
+    fn header(&self) -> &Header {
+        &self.header
+    }
+}
+
+#[derive(Debug)]
+pub struct Response {
+    header: Header,
+    status: Status,
+}
+
+impl Response {
+    pub const fn new(header: Header, status: Status) -> Self {
+        Self { header, status }
+    }
+
+    pub const fn status(&self) -> &Status {
+        &self.status
+    }
+}
+
+impl Frame<ID> for Response {
+    fn header(&self) -> &Header {
+        &self.header
+    }
+}

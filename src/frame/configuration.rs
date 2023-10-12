e38696c9ef5d418d518ad0dfa1eabfbfd04fc217
Richard Neumann <neumann@paulmann.de>
2023-10-12 10:43:14 +0200

    Implement getPolicy

--- src/frame/configuration.rs
+++ src/frame/configuration.rs
@@ -1,5 +1,6 @@
 mod add_endpoint;
 mod get_configuration_value;
+mod get_policy;
 mod read_attribute;
 mod set_configuration_value;
 mod set_policy;
---
+++ src/frame/configuration/get_policy.rs
@@ -0,0 +1,61 @@
+use crate::frame::header::Header;
+use crate::frame::Frame;
+use crate::policy;
+use crate::status::Status;
+use num_traits::ToPrimitive;
+
+const ID: u16 = 0x0056;
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Command {
+    header: Header,
+    policy_id: policy::Id,
+}
+
+impl Command {
+    pub const fn new(header: Header, policy_id: policy::Id) -> Self {
+        Self { header, policy_id }
+    }
+
+    pub const fn policy_id(&self) -> &policy::Id {
+        &self.policy_id
+    }
+}
+
+impl Frame<ID> for Command {
+    fn header(&self) -> &Header {
+        &self.header
+    }
+
+    fn parameters(&self) -> Vec<u8> {
+        vec![self
+            .policy_id
+            .to_u8()
+            .expect("could not convert policy ID to u8")]
+    }
+}
+
+pub struct Response {
+    header: Header,
+    status: Status,
+}
+
+impl Response {
+    pub const fn new(header: Header, status: Status) -> Self {
+        Self { header, status }
+    }
+
+    pub const fn status(&self) -> &Status {
+        &self.status
+    }
+}
+
+impl Frame<ID> for Response {
+    fn header(&self) -> &Header {
+        &self.header
+    }
+
+    fn parameters(&self) -> Vec<u8> {
+        vec![self.status.to_u8().expect("could not convert status to u8")]
+    }
+}

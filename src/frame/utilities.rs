02fedc347636cc11ea90059389e19c31984ae7ce
Richard Neumann <neumann@paulmann.de>
2023-10-12 13:33:09 +0200

    Implement invalidCommand

--- src/frame/utilities.rs
+++ src/frame/utilities.rs
@@ -1,2 +1,3 @@
 pub mod echo;
+pub mod invalid_command;
 pub mod nop;
---
+++ src/frame/utilities/invalid_command.rs
@@ -0,0 +1,32 @@
+use crate::frame::header::Header;
+use crate::frame::Frame;
+use crate::status::Status;
+use num_traits::ToPrimitive;
+
+const ID: u16 = 0x0058;
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Response {
+    header: Header,
+    reason: Status,
+}
+
+impl Response {
+    pub const fn new(header: Header, reason: Status) -> Self {
+        Self { header, reason }
+    }
+
+    pub const fn reason(&self) -> &Status {
+        &self.reason
+    }
+}
+
+impl Frame<ID> for Response {
+    fn header(&self) -> &Header {
+        &self.header
+    }
+
+    fn parameters(&self) -> Vec<u8> {
+        vec![self.reason.to_u8().expect("could not convert reason to u8")]
+    }
+}

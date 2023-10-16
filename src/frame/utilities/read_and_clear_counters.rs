de6b164dfc6e0caa9235c49d2359104a9e900b7b
Richard Neumann <neumann@paulmann.de>
2023-10-16 08:51:43 +0200

    Use Counter enum

--- src/frame/utilities/read_and_clear_counters.rs
+++ src/frame/utilities/read_and_clear_counters.rs
@@ -1,9 +1,9 @@
+use crate::counter::Counter;
 use crate::frame::header::{Control, Header};
 use crate::frame::Frame;
 use never::Never;
 
 const ID: u16 = 0x0065;
-const EMBER_COUNTER_TYPE_COUNT: usize = 40;
 
 /// Retrieves and clears Ember counters. See the EmberCounterType enumeration for the counter types.
 #[derive(Debug, Eq, PartialEq)]
@@ -35,14 +35,15 @@ impl Frame<ID> for Command {
 #[derive(Debug, Eq, PartialEq)]
 pub struct Response {
     header: Header,
-    values: [u16; EMBER_COUNTER_TYPE_COUNT],
+    values: [u16; Counter::TypeCount as usize],
 }
 
 impl Response {
+    #[must_use]
     pub const fn new(
         sequence: u8,
         control: Control,
-        values: [u16; EMBER_COUNTER_TYPE_COUNT],
+        values: [u16; Counter::TypeCount as usize],
     ) -> Self {
         Self {
             header: Header::for_frame::<ID>(sequence, control),
@@ -50,6 +51,7 @@ impl Response {
         }
     }
 
+    #[must_use]
     pub const fn values(&self) -> &[u16] {
         &self.values
     }
@@ -63,7 +65,7 @@ impl Frame<ID> for Response {
     }
 
     fn parameters(&self) -> Option<Self::Parameters> {
-        let mut parameters = Vec::with_capacity(2 * EMBER_COUNTER_TYPE_COUNT);
+        let mut parameters = Vec::with_capacity(2 * (Counter::TypeCount as usize));
         self.values
             .iter()
             .for_each(|value| parameters.extend_from_slice(&value.to_be_bytes()));

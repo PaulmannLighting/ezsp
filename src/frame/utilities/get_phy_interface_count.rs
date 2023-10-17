167229c5e5bb0b2e48c998cea901ce620b5d649a
Richard Neumann <neumann@paulmann.de>
2023-10-17 12:55:10 +0200

    Implement getPhyInterfaceCount

---
+++ src/frame/utilities/get_phy_interface_count.rs
@@ -0,0 +1,45 @@
+use std::array::IntoIter;
+use std::iter::{empty, Empty};
+
+pub const ID: u16 = 0x00FC;
+
+/// Returns the 16-bit node ID of the local node.
+#[derive(Debug, Eq, PartialEq)]
+pub struct Command;
+
+impl Command {
+    #[must_use]
+    pub const fn new() -> Self {
+        Self {}
+    }
+}
+
+impl IntoIterator for Command {
+    type Item = u8;
+    type IntoIter = Empty<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        empty()
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Response {
+    interface_count: u8,
+}
+
+impl Response {
+    #[must_use]
+    pub const fn new(interface_count: u8) -> Self {
+        Self { interface_count }
+    }
+}
+
+impl IntoIterator for Response {
+    type Item = u8;
+    type IntoIter = IntoIter<Self::Item, 1>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        [self.interface_count].into_iter()
+    }
+}

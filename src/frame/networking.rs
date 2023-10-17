52619f3f2f3c8d87aaeeec5c1b9de41f4ea1ad42
Richard Neumann <neumann@paulmann.de>
2023-10-17 16:22:13 +0200

    Implement networkState

--- src/frame/networking.rs
+++ src/frame/networking.rs
@@ -1,3 +1,4 @@
 pub mod network_init;
+pub mod network_state;
 pub mod set_manufacturer_code;
 pub mod set_power_descriptor;
---
+++ src/frame/networking/network_state.rs
@@ -0,0 +1,78 @@
+use crate::frame::Parameters;
+use crate::network::Status;
+use std::array::IntoIter;
+use std::io::Read;
+use std::iter::{empty, Empty};
+
+pub const ID: u16 = 0x0018;
+
+/// Returns a value indicating whether the node is joining, joined to, or leaving a network.
+#[derive(Debug, Eq, PartialEq)]
+pub struct Command;
+
+impl Command {
+    #[must_use]
+    pub const fn new() -> Self {
+        Self {}
+    }
+}
+
+impl IntoIterator for Command {
+    type Item = u8;
+    type IntoIter = Empty<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        empty()
+    }
+}
+
+impl Parameters<u16> for Command {
+    const FRAME_ID: u16 = ID;
+
+    fn read_from<R>(_: &mut R) -> anyhow::Result<Self>
+    where
+        R: Read,
+    {
+        Ok(Self {})
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Response {
+    status: Status,
+}
+
+impl Response {
+    #[must_use]
+    pub const fn new(status: Status) -> Self {
+        Self { status }
+    }
+
+    pub const fn status(&self) -> Status {
+        self.status
+    }
+}
+
+impl IntoIterator for Response {
+    type Item = u8;
+    type IntoIter = IntoIter<Self::Item, 1>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        [self.status.into()].into_iter()
+    }
+}
+
+impl Parameters<u16> for Response {
+    const FRAME_ID: u16 = ID;
+
+    fn read_from<R>(src: &mut R) -> anyhow::Result<Self>
+    where
+        R: Read,
+    {
+        let mut buffer @ [status] = [0; 1];
+        src.read_exact(&mut buffer)?;
+        Ok(Self {
+            status: Status::try_from(status)?,
+        })
+    }
+}

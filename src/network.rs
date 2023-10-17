94d9b75b5d94e913bd80d5a026bcf33e7d128228
Richard Neumann <neumann@paulmann.de>
2023-10-17 16:17:07 +0200

    Implement network::Status

--- src/network.rs
+++ src/network.rs
@@ -10,8 +10,8 @@ pub enum InitBitmask {
 }
 
 impl From<InitBitmask> for u16 {
-    fn from(network_init_bitmask: InitBitmask) -> Self {
-        network_init_bitmask
+    fn from(init_bitmask: InitBitmask) -> Self {
+        init_bitmask
             .to_u16()
             .expect("could not convert Units to u8")
     }
@@ -22,5 +22,28 @@ impl TryFrom<u16> for InitBitmask {
 
     fn try_from(value: u16) -> Result<Self, Self::Error> {
         Self::from_u16(value).ok_or_else(|| anyhow!("Invalid Units: {value:#04X}"))
+    }
+}
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, Ord, PartialOrd, FromPrimitive, ToPrimitive)]
+pub enum Status {
+    NoNetwork = 0x00,
+    JoiningNetwork = 0x01,
+    JoinedNetwork = 0x02,
+    JoinedNetworkNoParent = 0x03,
+    LeavingNetwork = 0x04,
+}
+
+impl From<Status> for u8 {
+    fn from(status: Status) -> Self {
+        status.to_u8().expect("could not convert Units to u8")
+    }
+}
+
+impl TryFrom<u8> for Status {
+    type Error = anyhow::Error;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        Self::from_u8(value).ok_or_else(|| anyhow!("Invalid Units: {value:#04X}"))
     }
 }

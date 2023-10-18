e65398f80444edab86a1ef39af4c7a0f7dc86e2f
Richard Neumann <neumann@paulmann.de>
2023-10-18 19:35:47 +0200

    Implement scanCompleteHandler

--- src/frame/networking.rs
+++ src/frame/networking.rs
@@ -2,6 +2,7 @@ pub mod energy_scan_result_handler;
 pub mod network_found_handler;
 pub mod network_init;
 pub mod network_state;
+pub mod scan_complete_handler;
 pub mod set_manufacturer_code;
 pub mod set_power_descriptor;
 pub mod stack_status_handler;
---
+++ src/frame/networking/scan_complete_handler.rs
@@ -0,0 +1,58 @@
+use crate::ember::Status;
+use crate::frame::Parameters;
+use std::io::Read;
+use std::iter::{once, Chain, Once};
+
+pub const ID: u16 = 0x001C;
+
+/// Returns the status of the current scan of type [`crate::ezsp::network::scan::Type`].
+///
+/// [`Status::Success`] signals that the scan has completed.
+/// Other error conditions signify a failure to scan on the channel specified.
+#[derive(Debug, Eq, PartialEq)]
+pub struct Response {
+    channel: u8,
+    status: Status,
+}
+
+impl Response {
+    #[must_use]
+    pub const fn new(channel: u8, status: Status) -> Self {
+        Self { channel, status }
+    }
+
+    #[must_use]
+    pub const fn channel(&self) -> u8 {
+        self.channel
+    }
+
+    #[must_use]
+    pub const fn status(&self) -> Status {
+        self.status
+    }
+}
+
+impl IntoIterator for Response {
+    type Item = u8;
+    type IntoIter = Chain<Once<Self::Item>, Once<Self::Item>>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        once(self.channel).chain(once(self.status.into()))
+    }
+}
+
+impl Parameters<u16> for Response {
+    const FRAME_ID: u16 = ID;
+
+    fn read_from<R>(src: &mut R) -> anyhow::Result<Self>
+    where
+        R: Read,
+    {
+        let mut buffer @ [channel, status] = [0; 2];
+        src.read_exact(&mut buffer)?;
+        Ok(Self {
+            channel,
+            status: Status::try_from(status)?,
+        })
+    }
+}

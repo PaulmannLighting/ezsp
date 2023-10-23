ea43669a05e1e1307dd566f8b63abbc3aaffc499
Richard Neumann <neumann@paulmann.de>
2023-10-23 13:01:27 +0200

    Implement getNetworkParameters

--- src/frame/parameters/networking.rs
+++ src/frame/parameters/networking.rs
@@ -7,6 +7,7 @@ pub mod energy_scan_result_handler;
 pub mod find_and_rejoin_network;
 pub mod find_unused_pan_id;
 pub mod form_network;
+pub mod get_network_parameters;
 pub mod join_network;
 pub mod join_network_directly;
 pub mod leave_network;
@@ -28,6 +29,7 @@ pub enum Command {
     FindAndRejoinNetwork(find_and_rejoin_network::Command),
     FindUnusedPanId(find_unused_pan_id::Command),
     FormNetwork(form_network::Command),
+    GetNetworkParameters(get_network_parameters::Command),
     JoinNetwork(join_network::Command),
     JoinNetworkDirectly(join_network_directly::Command),
     LeaveNetwork(leave_network::Command),
@@ -48,6 +50,7 @@ impl Command {
             Self::FindAndRejoinNetwork(_) => find_and_rejoin_network::ID,
             Self::FindUnusedPanId(_) => find_unused_pan_id::ID,
             Self::FormNetwork(_) => form_network::ID,
+            Self::GetNetworkParameters(_) => get_network_parameters::ID,
             Self::JoinNetwork(_) => join_network::ID,
             Self::JoinNetworkDirectly(_) => join_network_directly::ID,
             Self::LeaveNetwork(_) => leave_network::ID,
@@ -74,6 +77,9 @@ impl Writable for Command {
             }
             Self::FindUnusedPanId(find_unused_pan_id) => find_unused_pan_id.write_to(dst),
             Self::FormNetwork(form_network) => form_network.write_to(dst),
+            Self::GetNetworkParameters(get_network_parameters) => {
+                get_network_parameters.write_to(dst)
+            }
             Self::JoinNetwork(join_network) => join_network.write_to(dst),
             Self::JoinNetworkDirectly(join_network_directly) => join_network_directly.write_to(dst),
             Self::LeaveNetwork(leave_network) => leave_network.write_to(dst),
@@ -94,6 +100,7 @@ pub enum Response {
     FindAndRejoinNetwork(find_and_rejoin_network::Response),
     FindUnusedPanId(find_unused_pan_id::Response),
     FormNetwork(form_network::Response),
+    GetNetworkParameters(get_network_parameters::Response),
     JoinNetwork(join_network::Response),
     JoinNetworkDirectly(join_network_directly::Response),
     LeaveNetwork(leave_network::Response),
@@ -114,6 +121,7 @@ impl Response {
             Self::FindAndRejoinNetwork(_) => find_and_rejoin_network::ID,
             Self::FindUnusedPanId(_) => find_unused_pan_id::ID,
             Self::FormNetwork(_) => form_network::ID,
+            Self::GetNetworkParameters(_) => get_network_parameters::ID,
             Self::JoinNetwork(_) => join_network::ID,
             Self::JoinNetworkDirectly(_) => join_network_directly::ID,
             Self::LeaveNetwork(_) => leave_network::ID,
@@ -140,6 +148,9 @@ impl Writable for Response {
             }
             Self::FindUnusedPanId(find_unused_pan_id) => find_unused_pan_id.write_to(dst),
             Self::FormNetwork(form_network) => form_network.write_to(dst),
+            Self::GetNetworkParameters(get_network_parameters) => {
+                get_network_parameters.write_to(dst)
+            }
             Self::JoinNetwork(join_network) => join_network.write_to(dst),
             Self::JoinNetworkDirectly(join_network_directly) => join_network_directly.write_to(dst),
             Self::LeaveNetwork(leave_network) => leave_network.write_to(dst),
---
+++ src/frame/parameters/networking/get_network_parameters.rs
@@ -0,0 +1,99 @@
+use crate::ember::network::parameters::Parameters;
+use crate::ember::node::Type;
+use crate::ember::Status;
+use crate::read_write::Readable;
+use rw_exact_ext::ReadExactExt;
+use std::io::Read;
+use std::iter::{empty, once, Chain, Empty, Once};
+
+pub const ID: u16 = 0x0028;
+
+/// Returns the current network parameters.
+#[derive(Debug, Eq, PartialEq)]
+pub struct Command;
+
+impl Command {
+    #[must_use]
+    pub const fn new() -> Self {
+        Self {}
+    }
+}
+
+impl IntoIterator for Command {
+    type Item = u8;
+    type IntoIter = Empty<Self::Item>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        empty()
+    }
+}
+
+impl Readable for Command {
+    fn read_from<R>(_: &mut R) -> anyhow::Result<Self>
+    where
+        R: Read,
+    {
+        Ok(Self {})
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Response {
+    status: Status,
+    node_type: Type,
+    parameters: Parameters,
+}
+
+impl Response {
+    #[must_use]
+    pub const fn new(status: Status, node_type: Type, parameters: Parameters) -> Self {
+        Self {
+            status,
+            node_type,
+            parameters,
+        }
+    }
+
+    #[must_use]
+    pub const fn status(&self) -> Status {
+        self.status
+    }
+
+    #[must_use]
+    pub const fn node_type(&self) -> Type {
+        self.node_type
+    }
+
+    #[must_use]
+    pub const fn parameters(&self) -> &Parameters {
+        &self.parameters
+    }
+}
+
+impl IntoIterator for Response {
+    type Item = u8;
+    type IntoIter =
+        Chain<Chain<Once<Self::Item>, Once<Self::Item>>, <Parameters as IntoIterator>::IntoIter>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        once(self.status.into())
+            .chain(once(self.node_type.into()))
+            .chain(self.parameters)
+    }
+}
+
+impl Readable for Response {
+    fn read_from<R>(src: &mut R) -> anyhow::Result<Self>
+    where
+        R: Read,
+    {
+        let status: u8 = src.read_num_be()?;
+        let node_type: u8 = src.read_num_be()?;
+        let parameters = Parameters::read_from(src)?;
+        Ok(Self {
+            status: status.try_into()?,
+            node_type: node_type.try_into()?,
+            parameters,
+        })
+    }
+}

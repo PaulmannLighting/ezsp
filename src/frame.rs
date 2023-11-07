0dd4d897ee34cc86384c6b220b731aea69d3b0c1
Richard Neumann <neumann@paulmann.de>
2023-11-07 12:52:40 +0100

    Add mockups of command, response and callback enumerations

--- src/frame.rs
+++ src/frame.rs
@@ -1,3 +1,22 @@
 pub mod header;
 pub mod legacy_parameters;
 pub mod parameters;
+
+pub use header::{Header, LegacyHeader};
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum Command {
+    AddEndpoint(Header, parameters::configuration::add_endpoint::Command),
+    //todo!()
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum Response {
+    AddEndpoint(Header, parameters::configuration::add_endpoint::Response),
+    //todo!()
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum Callback {
+    //todo!()
+}

d1b14f63ecdc7983813ceebcdafec36e10a535de
Richard Neumann <neumann@paulmann.de>
2023-11-08 11:05:21 +0100

    Update error

--- src/error.rs
+++ src/error.rs
@@ -1,14 +1,14 @@
-pub use crate::ember::error::Error as EmberError;
-pub use crate::ezsp::error::Error as EzspError;
 use std::fmt::{Display, Formatter};
 
 #[derive(Debug)]
 pub enum Error {
     AshError(ashv2::Error),
-    Ember(EmberError),
-    Ezsp(EzspError),
+    Deserialization(crate::serde::deserializer::Error),
+    Ember(crate::ember::error::Error),
+    Ezsp(crate::ezsp::error::Error),
     InvalidSize { expected: usize, found: usize },
     Io(std::io::Error),
+    Serialization(crate::serde::serializer::Error),
     SiliconLabs(siliconlabs::Error),
 }
 
@@ -16,12 +16,14 @@ impl Display for Error {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::AshError(error) => Display::fmt(error, f),
+            Self::Deserialization(error) => Display::fmt(error, f),
             Self::Ember(error) => Display::fmt(error, f),
             Self::Ezsp(error) => Display::fmt(error, f),
             Self::InvalidSize { expected, found } => {
                 write!(f, "Expected {expected} bytes, but found {found} bytes.")
             }
             Self::Io(error) => Display::fmt(error, f),
+            Self::Serialization(error) => Display::fmt(error, f),
             Self::SiliconLabs(error) => Display::fmt(error, f),
         }
     }
@@ -31,11 +33,16 @@ impl std::error::Error for Error {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match self {
             Self::AshError(error) => Some(error),
+            Self::Deserialization(error) => Some(error),
             Self::Ember(error) => Some(error),
             Self::Ezsp(error) => Some(error),
+            Self::InvalidSize {
+                expected: _,
+                found: _,
+            } => None,
             Self::Io(error) => Some(error),
+            Self::Serialization(error) => Some(error),
             Self::SiliconLabs(error) => Some(error),
-            _ => None,
         }
     }
 }
@@ -46,14 +53,20 @@ impl From<ashv2::Error> for Error {
     }
 }
 
-impl From<EmberError> for Error {
-    fn from(error: EmberError) -> Self {
+impl From<crate::serde::deserializer::Error> for Error {
+    fn from(error: crate::serde::deserializer::Error) -> Self {
+        Self::Deserialization(error)
+    }
+}
+
+impl From<crate::ember::error::Error> for Error {
+    fn from(error: crate::ember::error::Error) -> Self {
         Self::Ember(error)
     }
 }
 
-impl From<EzspError> for Error {
-    fn from(error: EzspError) -> Self {
+impl From<crate::ezsp::error::Error> for Error {
+    fn from(error: crate::ezsp::error::Error) -> Self {
         Self::Ezsp(error)
     }
 }
@@ -61,6 +74,12 @@ impl From<EzspError> for Error {
 impl From<std::io::Error> for Error {
     fn from(error: std::io::Error) -> Self {
         Self::Io(error)
+    }
+}
+
+impl From<crate::serde::serializer::Error> for Error {
+    fn from(error: crate::serde::serializer::Error) -> Self {
+        Self::Serialization(error)
     }
 }
 

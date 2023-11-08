1fd210acb6fd9b558b0003d3b363d932c4032bb8
Richard Neumann <neumann@paulmann.de>
2023-11-08 17:14:12 +0100

    Implement serializer and deserializer

--- src/serde/deserializer/error.rs
+++ src/serde/deserializer/error.rs
@@ -5,14 +5,16 @@ pub type Result<T> = std::result::Result<T, Error>;
 #[derive(Debug)]
 pub enum Error {
     Custom(Box<dyn std::error::Error>),
-    UnexpectedEof,
+    ResidualBytes,
+    UnexpectedEndOfStream,
 }
 
 impl Display for Error {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Custom(error) => Display::fmt(error, f),
-            Self::UnexpectedEof => write!(f, "unexpected end of file"),
+            Self::ResidualBytes => write!(f, "residual bytes"),
+            Self::UnexpectedEndOfStream => write!(f, "unexpected end of stream"),
         }
     }
 }
@@ -21,12 +23,12 @@ impl std::error::Error for Error {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match self {
             Self::Custom(error) => Some(error.as_ref()),
-            Self::UnexpectedEof => None,
+            Self::ResidualBytes | Self::UnexpectedEndOfStream => None,
         }
     }
 }
 
-impl serde::ser::Error for Error {
+impl serde::de::Error for Error {
     fn custom<T>(msg: T) -> Self
     where
         T: Display,

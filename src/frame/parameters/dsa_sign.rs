7279b3bc0b2d27876993632fefa6947934c62fa4
Richard Neumann <neumann@paulmann.de>
2023-11-13 08:05:33 +0100

    Implement dsa_sign.rs

--- src/frame/parameters/dsa_sign.rs
+++ src/frame/parameters/dsa_sign.rs
@@ -1,10 +1,11 @@
+use crate::ember::Status;
+use crate::types::ByteSizedVec;
 use le_stream::derive::{FromLeBytes, ToLeBytes};
-use crate::types::{EmberStatus};
 
 pub const ID: u16 = 0x00A6;
 
 #[derive(Debug, Eq, PartialEq, FromLeBytes, ToLeBytes)]
-pub struct Command{
+pub struct Command {
     message_length: u8,
     message_contents: ByteSizedVec<u8>,
 }
@@ -12,7 +13,10 @@ pub struct Command{
 impl Command {
     #[must_use]
     pub const fn new(message_length: u8, message_contents: ByteSizedVec<u8>) -> Self {
-        Self { message_length, message_contents }
+        Self {
+            message_length,
+            message_contents,
+        }
     }
 
     #[must_use]
@@ -20,26 +24,25 @@ impl Command {
         self.message_length
     }
 
-
     #[must_use]
-    pub const fn message_contents(&self) -> ByteSizedVec<u8> {
-        self.message_contents
+    pub const fn message_contents(&self) -> &ByteSizedVec<u8> {
+        &self.message_contents
     }
 }
 
 #[derive(Debug, Eq, PartialEq, FromLeBytes, ToLeBytes)]
-pub struct Response{
-    status: EmberStatus,
+pub struct Response {
+    status: u8,
 }
 
 impl Response {
-    #[must_use]
-    pub const fn new(status: EmberStatus) -> Self {
-        Self { status }
+    pub fn new(status: Status) -> Self {
+        Self {
+            status: status.into(),
+        }
     }
 
-    #[must_use]
-    pub const fn status(&self) -> EmberStatus {
-        self.status
+    pub fn status(&self) -> Result<Status, u8> {
+        Status::try_from(self.status)
     }
 }

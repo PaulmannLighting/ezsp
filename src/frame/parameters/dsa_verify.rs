df1c08d8d561497169351cb621b3e2fcb06a9327
Richard Neumann <neumann@paulmann.de>
2023-11-13 07:50:09 +0100

    Implement dsa_verify.rs

--- src/frame/parameters/dsa_verify.rs
+++ src/frame/parameters/dsa_verify.rs
@@ -1,52 +1,62 @@
+use crate::ember::types::{CertificateData, MessageDigest, SignatureData};
+use crate::ember::Status;
 use le_stream::derive::{FromLeBytes, ToLeBytes};
-use crate::types::{EmberSignatureData,EmberMessageDigest,EmberCertificateData,EmberStatus};
+use num_traits::FromPrimitive;
 
 pub const ID: u16 = 0x00A3;
 
 #[derive(Debug, Eq, PartialEq, FromLeBytes, ToLeBytes)]
-pub struct Command{
-    digest: EmberMessageDigest,
-    signer_certificate: EmberCertificateData,
-    received_sig: EmberSignatureData,
+pub struct Command {
+    digest: MessageDigest,
+    signer_certificate: CertificateData,
+    received_sig: SignatureData,
 }
 
 impl Command {
     #[must_use]
-    pub const fn new(digest: EmberMessageDigest, signer_certificate: EmberCertificateData, received_sig: EmberSignatureData) -> Self {
-        Self { digest, signer_certificate, received_sig }
+    pub const fn new(
+        digest: MessageDigest,
+        signer_certificate: CertificateData,
+        received_sig: SignatureData,
+    ) -> Self {
+        Self {
+            digest,
+            signer_certificate,
+            received_sig,
+        }
     }
 
     #[must_use]
-    pub const fn digest(&self) -> EmberMessageDigest {
-        self.digest
+    pub const fn digest(&self) -> &MessageDigest {
+        &self.digest
     }
 
-
     #[must_use]
-    pub const fn signer_certificate(&self) -> EmberCertificateData {
-        self.signer_certificate
+    pub const fn signer_certificate(&self) -> &CertificateData {
+        &self.signer_certificate
     }
 
-
     #[must_use]
-    pub const fn received_sig(&self) -> EmberSignatureData {
-        self.received_sig
+    pub const fn received_sig(&self) -> &SignatureData {
+        &self.received_sig
     }
 }
 
 #[derive(Debug, Eq, PartialEq, FromLeBytes, ToLeBytes)]
-pub struct Response{
-    status: EmberStatus,
+pub struct Response {
+    status: u8,
 }
 
 impl Response {
     #[must_use]
-    pub const fn new(status: EmberStatus) -> Self {
-        Self { status }
+    pub fn new(status: Status) -> Self {
+        Self {
+            status: status.into(),
+        }
     }
 
     #[must_use]
-    pub const fn status(&self) -> EmberStatus {
-        self.status
+    pub const fn status(&self) -> Option<Status> {
+        Status::from_u8(self.status)
     }
 }

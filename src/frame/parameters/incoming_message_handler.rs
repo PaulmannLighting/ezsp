0bb752ba9bf123756c9fe929643292c6d213b765
Richard Neumann <neumann@paulmann.de>
2023-11-14 08:21:08 +0100

    [WIP] Implement incomingMessageHandler()

--- src/frame/parameters/incoming_message_handler.rs
+++ src/frame/parameters/incoming_message_handler.rs
@@ -1,5 +1,8 @@
+use crate::ember::aps::Frame;
+use crate::ember::message::Incoming;
+use crate::ember::NodeId;
+use crate::types::ByteSizedVec;
 use le_stream::derive::{FromLeBytes, ToLeBytes};
-use crate::types::{EmberApsFrame,EmberNodeId,int8s,EmberIncomingMessageType};
 
 pub const ID: u16 = 0x0045;
 
@@ -9,79 +12,87 @@ pub struct Command;
 impl Command {
     #[must_use]
     pub const fn new() -> Self {
-        Self {  }
+        Self {}
     }
 }
 
 #[derive(Debug, Eq, PartialEq, FromLeBytes, ToLeBytes)]
-pub struct Response{
-    typ: EmberIncomingMessageType,
-    aps_frame: EmberApsFrame,
+pub struct Response {
+    typ: u8,
+    aps_frame: Frame,
     last_hop_lqi: u8,
-    last_hop_rssi: int8s,
-    sender: EmberNodeId,
+    last_hop_rssi: i8,
+    sender: NodeId,
     binding_index: u8,
     address_index: u8,
-    message_length: u8,
     message_contents: ByteSizedVec<u8>,
 }
 
 impl Response {
     #[must_use]
-    pub const fn new(typ: EmberIncomingMessageType, aps_frame: EmberApsFrame, last_hop_lqi: u8, last_hop_rssi: int8s, sender: EmberNodeId, binding_index: u8, address_index: u8, message_length: u8, message_contents: ByteSizedVec<u8>) -> Self {
-        Self { typ, aps_frame, last_hop_lqi, last_hop_rssi, sender, binding_index, address_index, message_length, message_contents }
+    pub fn new(
+        typ: Incoming,
+        aps_frame: Frame,
+        last_hop_lqi: u8,
+        last_hop_rssi: i8,
+        sender: NodeId,
+        binding_index: u8,
+        address_index: u8,
+        message_contents: ByteSizedVec<u8>,
+    ) -> Self {
+        Self {
+            typ: typ.into(),
+            aps_frame,
+            last_hop_lqi,
+            last_hop_rssi,
+            sender,
+            binding_index,
+            address_index,
+            message_contents,
+        }
+    }
+
+    pub fn typ(&self) -> Result<Incoming, u8> {
+        Incoming::try_from(self.typ)
     }
 
     #[must_use]
-    pub const fn typ(&self) -> EmberIncomingMessageType {
-        self.typ
+    pub const fn aps_frame(&self) -> &Frame {
+        &self.aps_frame
     }
-
-
-    #[must_use]
-    pub const fn aps_frame(&self) -> EmberApsFrame {
-        self.aps_frame
-    }
-
 
     #[must_use]
     pub const fn last_hop_lqi(&self) -> u8 {
         self.last_hop_lqi
     }
 
-
     #[must_use]
-    pub const fn last_hop_rssi(&self) -> int8s {
+    pub const fn last_hop_rssi(&self) -> i8 {
         self.last_hop_rssi
     }
 
-
     #[must_use]
-    pub const fn sender(&self) -> EmberNodeId {
+    pub const fn sender(&self) -> NodeId {
         self.sender
     }
-
 
     #[must_use]
     pub const fn binding_index(&self) -> u8 {
         self.binding_index
     }
 
-
     #[must_use]
     pub const fn address_index(&self) -> u8 {
         self.address_index
     }
-
 
     #[must_use]
     pub const fn message_length(&self) -> u8 {
         self.message_length
     }
 
-
     #[must_use]
-    pub const fn message_contents(&self) -> ByteSizedVec<u8> {
-        self.message_contents
+    pub const fn message_contents(&self) -> &ByteSizedVec<u8> {
+        &self.message_contents
     }
 }

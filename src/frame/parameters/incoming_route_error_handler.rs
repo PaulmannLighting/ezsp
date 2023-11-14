cd6ddb6414c8bc77da2e92bad920cc60d2e6b22a
Richard Neumann <neumann@paulmann.de>
2023-11-14 08:22:39 +0100

    [WIP] Implement incoming_route_error_handler

--- src/frame/parameters/incoming_route_error_handler.rs
+++ src/frame/parameters/incoming_route_error_handler.rs
@@ -1,5 +1,5 @@
+use crate::ember::{NodeId, Status};
 use le_stream::derive::{FromLeBytes, ToLeBytes};
-use crate::types::{EmberNodeId,EmberStatus};
 
 pub const ID: u16 = 0x0080;
 
@@ -9,30 +9,31 @@ pub struct Command;
 impl Command {
     #[must_use]
     pub const fn new() -> Self {
-        Self {  }
+        Self {}
     }
 }
 
 #[derive(Debug, Eq, PartialEq, FromLeBytes, ToLeBytes)]
-pub struct Response{
-    status: EmberStatus,
-    target: EmberNodeId,
+pub struct Response {
+    status: u8,
+    target: NodeId,
 }
 
 impl Response {
     #[must_use]
-    pub const fn new(status: EmberStatus, target: EmberNodeId) -> Self {
-        Self { status, target }
+    pub fn new(status: Status, target: NodeId) -> Self {
+        Self {
+            status: status.into(),
+            target,
+        }
+    }
+
+    pub fn status(&self) -> Result<Status, u8> {
+        Status::try_from(self.status)
     }
 
     #[must_use]
-    pub const fn status(&self) -> EmberStatus {
-        self.status
-    }
-
-
-    #[must_use]
-    pub const fn target(&self) -> EmberNodeId {
+    pub const fn target(&self) -> NodeId {
         self.target
     }
 }

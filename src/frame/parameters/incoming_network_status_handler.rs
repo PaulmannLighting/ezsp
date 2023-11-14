2e37c3d39ec6a5b22e6f7089c642499586460c16
Richard Neumann <neumann@paulmann.de>
2023-11-14 08:21:38 +0100

    [WIP] Implement incoming_network_status_handler

--- src/frame/parameters/incoming_network_status_handler.rs
+++ src/frame/parameters/incoming_network_status_handler.rs
@@ -1,5 +1,5 @@
+use crate::ember::NodeId;
 use le_stream::derive::{FromLeBytes, ToLeBytes};
-use crate::types::{EmberNodeId};
 
 pub const ID: u16 = 0x00C4;
 
@@ -9,19 +9,19 @@ pub struct Command;
 impl Command {
     #[must_use]
     pub const fn new() -> Self {
-        Self {  }
+        Self {}
     }
 }
 
 #[derive(Debug, Eq, PartialEq, FromLeBytes, ToLeBytes)]
-pub struct Response{
+pub struct Response {
     error_code: u8,
-    target: EmberNodeId,
+    target: NodeId,
 }
 
 impl Response {
     #[must_use]
-    pub const fn new(error_code: u8, target: EmberNodeId) -> Self {
+    pub const fn new(error_code: u8, target: NodeId) -> Self {
         Self { error_code, target }
     }
 
@@ -30,9 +30,8 @@ impl Response {
         self.error_code
     }
 
-
     #[must_use]
-    pub const fn target(&self) -> EmberNodeId {
+    pub const fn target(&self) -> NodeId {
         self.target
     }
 }

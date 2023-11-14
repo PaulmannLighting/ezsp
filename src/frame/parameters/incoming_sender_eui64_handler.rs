276ce8089ee618ae1c256f7675dc15ce3fb50a55
Richard Neumann <neumann@paulmann.de>
2023-11-14 08:23:01 +0100

    [WIP] Implement incoming_sender_eui64_handler

--- src/frame/parameters/incoming_sender_eui64_handler.rs
+++ src/frame/parameters/incoming_sender_eui64_handler.rs
@@ -1,5 +1,5 @@
+use crate::ember::Eui64;
 use le_stream::derive::{FromLeBytes, ToLeBytes};
-use crate::types::{EmberEUI64};
 
 pub const ID: u16 = 0x0062;
 
@@ -9,23 +9,23 @@ pub struct Command;
 impl Command {
     #[must_use]
     pub const fn new() -> Self {
-        Self {  }
+        Self {}
     }
 }
 
 #[derive(Debug, Eq, PartialEq, FromLeBytes, ToLeBytes)]
-pub struct Response{
-    sender_eui64: EmberEUI64,
+pub struct Response {
+    sender_eui64: Eui64,
 }
 
 impl Response {
     #[must_use]
-    pub const fn new(sender_eui64: EmberEUI64) -> Self {
+    pub const fn new(sender_eui64: Eui64) -> Self {
         Self { sender_eui64 }
     }
 
     #[must_use]
-    pub const fn sender_eui64(&self) -> EmberEUI64 {
+    pub const fn sender_eui64(&self) -> Eui64 {
         self.sender_eui64
     }
 }

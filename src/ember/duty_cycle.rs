8697d2ad9295fb763c2d3293a1e8aefc3a710222
Richard Neumann <neumann@paulmann.de>
2023-11-17 08:47:39 +0100

    [WIP] Implement duty_cycle.Limits

--- src/ember/duty_cycle.rs
+++ src/ember/duty_cycle.rs
@@ -1,3 +1,4 @@
+use le_stream::derive::{FromLeBytes, ToLeBytes};
 use num_derive::{FromPrimitive, ToPrimitive};
 use num_traits::{FromPrimitive, ToPrimitive};
 
@@ -23,5 +24,31 @@ impl TryFrom<u8> for State {
 
     fn try_from(duty_cycle_state: u8) -> Result<Self, Self::Error> {
         Self::from_u8(duty_cycle_state).ok_or(duty_cycle_state)
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, FromLeBytes, ToLeBytes)]
+pub struct Limits {
+    vendor_id: u16,
+    vendor_string: [u8; 7],
+}
+
+impl Limits {
+    #[must_use]
+    pub fn new(vendor_id: u16, vendor_string: [u8; 7]) -> Self {
+        Self {
+            vendor_id,
+            vendor_string,
+        }
+    }
+
+    #[must_use]
+    pub const fn vendor_id(&self) -> u16 {
+        self.vendor_id
+    }
+
+    #[must_use]
+    pub const fn vendor_string(&self) -> &[u8; 7] {
+        &self.vendor_string
     }
 }

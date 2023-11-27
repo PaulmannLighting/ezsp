c28335131d386f6b15c39c5fe6959c387328883e
Richard Neumann <neumann@paulmann.de>
2023-11-27 15:07:47 +0100

    Implement NetworkOperation

---
+++ src/ember/zll/network_operation.rs
@@ -0,0 +1,24 @@
+use num_derive::{FromPrimitive, ToPrimitive};
+use num_traits::{FromPrimitive, ToPrimitive};
+
+#[derive(Debug, Clone, Copy, Ord, PartialOrd, Eq, PartialEq, FromPrimitive, ToPrimitive)]
+pub enum NetworkOperation {
+    FormNetwork = 0x00,
+    JoinTarget = 0x01,
+}
+
+impl From<NetworkOperation> for u8 {
+    fn from(network_operation: NetworkOperation) -> Self {
+        network_operation
+            .to_u8()
+            .expect("could not convert NetworkOperation to u8")
+    }
+}
+
+impl TryFrom<u8> for NetworkOperation {
+    type Error = u8;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        Self::from_u8(value).ok_or(value)
+    }
+}

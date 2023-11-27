3eded36c5212662be10db0fbb5c06d785a2e4763
Richard Neumann <neumann@paulmann.de>
2023-11-27 12:36:20 +0100

    [WIP] Implement further parameters
    Last: set_security_key

--- src/frame/parameters/set_security_key.rs
+++ src/frame/parameters/set_security_key.rs
@@ -1,45 +1,46 @@
+use crate::ember::key::Data;
+use crate::ezsp::{SecurityType, Status};
 use le_stream::derive::{FromLeBytes, ToLeBytes};
-use crate::types::{EmberKeyData,EzspStatus,SecureEzspSecurityType};
 
 pub const ID: u16 = 0x00CA;
 
 #[derive(Debug, Eq, PartialEq, FromLeBytes, ToLeBytes)]
-pub struct Command{
-    key: EmberKeyData,
-    security_type: SecureEzspSecurityType,
+pub struct Command {
+    key: Data,
+    security_type: SecurityType,
 }
 
 impl Command {
     #[must_use]
-    pub const fn new(key: EmberKeyData, security_type: SecureEzspSecurityType) -> Self {
+    pub const fn new(key: Data, security_type: SecurityType) -> Self {
         Self { key, security_type }
     }
 
     #[must_use]
-    pub const fn key(&self) -> EmberKeyData {
+    pub const fn key(&self) -> Data {
         self.key
     }
 
-
     #[must_use]
-    pub const fn security_type(&self) -> SecureEzspSecurityType {
+    pub const fn security_type(&self) -> SecurityType {
         self.security_type
     }
 }
 
 #[derive(Debug, Eq, PartialEq, FromLeBytes, ToLeBytes)]
-pub struct Response{
-    status: EzspStatus,
+pub struct Response {
+    status: u8,
 }
 
 impl Response {
     #[must_use]
-    pub const fn new(status: EzspStatus) -> Self {
-        Self { status }
+    pub fn new(status: Status) -> Self {
+        Self {
+            status: status.into(),
+        }
     }
 
-    #[must_use]
-    pub const fn status(&self) -> EzspStatus {
-        self.status
+    pub fn status(&self) -> Result<Status, u8> {
+        Status::try_from(self.status)
     }
 }

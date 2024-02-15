89a30cf9078107802c31929db1262fda507d7036
Richard Neumann <neumann@paulmann.de>
2024-02-15 10:16:52 +0100

    Update protocol implementation

--- src/protocol.rs
+++ src/protocol.rs
@@ -1,25 +1,32 @@
 mod counters;
+mod error;
 
-use counters::Counters;
-use serialport::SerialPort;
+use error::Error;
+use le_stream::{FromLeBytes, ToLeBytes};
 
 // XXX: Mockup
-pub trait Transaction {}
+pub trait Transaction {
+    type Response: FromLeBytes;
+
+    fn command(&self) -> impl ToLeBytes;
+}
 
 pub trait Protocol {
     const NAME: &'static str;
 
-    fn start(serial_port: impl SerialPort);
+    async fn write<T>(&mut self, packet: T) -> Result<(), Error>
+    where
+        T: ToLeBytes;
 
-    fn connect(&self);
+    async fn read<T>(&mut self) -> Result<T, Error>
+    where
+        T: FromLeBytes;
 
-    fn stop(&self); // setClosing()
-
-    fn close(&self);
-
-    fn enqueue(&self, frame: &[u8]);
-
-    fn send_transaction(&self, transaction: impl Transaction);
-
-    fn counters(&self) -> Counters;
+    async fn send_transaction<T>(&mut self, transaction: T) -> Result<T::Response, Error>
+    where
+        T: Transaction,
+    {
+        self.write(transaction.command()).await?;
+        self.read().await
+    }
 }

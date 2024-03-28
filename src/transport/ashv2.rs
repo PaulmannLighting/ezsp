95f1b0474e22933d25112d2e8d563ab8bdf9c178
Richard Neumann <neumann@paulmann.de>
2024-03-28 19:45:17 +0100

    Use native TTYPort

--- src/transport/ashv2.rs
+++ src/transport/ashv2.rs
@@ -7,7 +7,7 @@ use std::sync::Arc;
 use ashv2::Host;
 use le_stream::ToLeBytes;
 use log::debug;
-use serialport::SerialPort;
+use serialport::{SerialPort, TTYPort};
 
 use crate::ember;
 use crate::ember::binding::TableEntry;
@@ -44,14 +44,13 @@ impl<'a> Ashv2<'a> {
     ///
     /// # Errors
     /// Returns an [`ashv2::Error`] if spawning fails.
-    pub fn spawn<S>(
-        serial_port: S,
+    pub fn spawn(
+        serial_port: TTYPort,
         control: Control,
         callback: Option<Sender<Arc<[u8]>>>,
     ) -> Result<Self, ashv2::Error>
     where
         Self: 'static,
-        S: SerialPort + 'a,
     {
         Ok(Self {
             host: Host::spawn(serial_port, callback)?,

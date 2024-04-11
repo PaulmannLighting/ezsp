d472edc5eabb6163822a6dea41cc94f96fdacc4d
Richard Neumann <neumann@paulmann.de>
2024-04-11 08:04:41 +0200

    Update callback implementation

--- src/frame/parameters/callback.rs
+++ src/frame/parameters/callback.rs
@@ -1,3 +1,4 @@
+use crate::frame::Parameter;
 use le_stream::derive::{FromLeBytes, ToLeBytes};
 
 const ID: u16 = 0x0006;
@@ -12,12 +13,7 @@ impl Command {
     }
 }
 
-#[derive(Debug, Eq, PartialEq, FromLeBytes, ToLeBytes)]
-pub struct Response;
-
-impl Response {
-    #[must_use]
-    pub const fn new() -> Self {
-        Self {}
-    }
+impl Parameter for Command {
+    type Id = u16;
+    const ID: Self::Id = ID;
 }

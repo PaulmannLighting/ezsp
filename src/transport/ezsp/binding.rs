62820ba5c28ee07602d3effe59680b39c617916d
Richard Neumann <neumann@paulmann.de>
2024-04-15 13:03:43 +0200

    Add docstrings

--- src/transport/ezsp/binding.rs
+++ src/transport/ezsp/binding.rs
@@ -26,11 +26,21 @@ pub trait Binding: Transport {
     /// [`TableEntry`] has the value [`Type::Unused`].
     fn binding_is_active(&self, index: u8) -> impl Future<Output = Result<bool, Error>> + Send;
 
+    /// Returns the node ID for the binding's destination, if the ID is known.
+    /// If a message is sent using the binding and the destination's ID is not known,
+    /// the stack will discover the ID by broadcasting a ZDO address request.
+    /// The application can avoid the need for this discovery by using [`Self::set_binding_remote_node_id`]
+    /// when it knows the correct ID via some other means.
+    /// The destination's node ID is forgotten when the binding is changed,
+    /// when the local node reboots or, much more rarely,
+    /// when the destination node changes its ID in response to an ID conflict.
     fn get_binding_remote_node_id(
         &self,
         index: u8,
     ) -> impl Future<Output = Result<NodeId, Error>> + Send;
 
+    /// Set the node ID for the binding's destination.
+    /// See [`Self::get_binding_remote_node_id`] for a description.
     fn set_binding_remote_node_id(
         &self,
         index: u8,

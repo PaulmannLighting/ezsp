3051f9f981cfcd3445b8e45c527692d3bfef4654
Richard Neumann <neumann@paulmann.de>
2024-04-16 15:31:23 +0200

    Strip down invalid_command

--- src/frame/parameters/invalid_command.rs
+++ src/frame/parameters/invalid_command.rs
@@ -1,32 +1,21 @@
 use crate::ezsp::Status;
+use crate::frame::Parameter;
 use le_stream::derive::{FromLeBytes, ToLeBytes};
 
 const ID: u16 = 0x0058;
 
-#[derive(Debug, Eq, PartialEq, FromLeBytes, ToLeBytes)]
-pub struct Command;
-
-impl Command {
-    #[must_use]
-    pub const fn new() -> Self {
-        Self {}
-    }
-}
-
-#[derive(Debug, Eq, PartialEq, FromLeBytes, ToLeBytes)]
+#[derive(Clone, Debug, Eq, PartialEq, FromLeBytes, ToLeBytes)]
 pub struct Response {
     reason: u8,
 }
 
 impl Response {
-    #[must_use]
-    pub fn new(reason: Status) -> Self {
-        Self {
-            reason: reason.into(),
-        }
-    }
-
     pub fn reason(&self) -> Result<Status, u8> {
         Status::try_from(self.reason)
     }
+}
+
+impl Parameter for Response {
+    type Id = u16;
+    const ID: Self::Id = ID;
 }

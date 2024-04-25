cc902605d8c1d2c1b92482e3a38087d18709726a
Richard Neumann <neumann@paulmann.de>
2024-04-25 14:01:40 +0200

    Refactor get_address_table_remote_eui64

--- src/frame/parameters/messaging/get_address_table_remote_eui64.rs
+++ src/frame/parameters/messaging/get_address_table_remote_eui64.rs
@@ -1,6 +1,7 @@
+use le_stream::derive::{FromLeBytes, ToLeBytes};
+
 use crate::ember::Eui64;
 use crate::frame::Parameter;
-use le_stream::derive::{FromLeBytes, ToLeBytes};
 
 const ID: u16 = 0x005E;
 

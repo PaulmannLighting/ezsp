81df0b7830c888dd30681b844c86d19319906baa
Richard Neumann <neumann@paulmann.de>
2024-04-25 14:30:42 +0200

    Rewrite replace_address_table_entry

--- src/frame/parameters/messaging/replace_address_table_entry.rs
+++ src/frame/parameters/messaging/replace_address_table_entry.rs
@@ -1,9 +1,13 @@
-use crate::ember::{Eui64, NodeId, Status};
 use le_stream::derive::{FromLeBytes, ToLeBytes};
+
+use crate::ember::{Eui64, NodeId, Status};
+use crate::error::Resolve;
+use crate::frame::Parameter;
+use crate::Error;
 
 const ID: u16 = 0x0082;
 
-#[derive(Debug, Eq, PartialEq, FromLeBytes, ToLeBytes)]
+#[derive(Debug, Eq, PartialEq, ToLeBytes)]
 pub struct Command {
     address_table_index: u8,
     new_eui64: Eui64,
@@ -26,56 +30,21 @@ impl Command {
             new_extended_timeout,
         }
     }
-
-    #[must_use]
-    pub const fn address_table_index(&self) -> u8 {
-        self.address_table_index
-    }
-
-    #[must_use]
-    pub const fn new_eui64(&self) -> Eui64 {
-        self.new_eui64
-    }
-
-    #[must_use]
-    pub const fn new_id(&self) -> NodeId {
-        self.new_id
-    }
-
-    #[must_use]
-    pub const fn new_extended_timeout(&self) -> bool {
-        self.new_extended_timeout
-    }
 }
 
-#[derive(Debug, Eq, PartialEq, FromLeBytes, ToLeBytes)]
-pub struct Response {
-    status: u8,
+impl Parameter for Command {
+    type Id = u16;
+    const ID: Self::Id = ID;
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, FromLeBytes)]
+pub struct Payload {
     old_eui64: Eui64,
     old_id: NodeId,
     old_extended_timeout: bool,
 }
 
-impl Response {
-    #[must_use]
-    pub fn new(
-        status: Status,
-        old_eui64: Eui64,
-        old_id: NodeId,
-        old_extended_timeout: bool,
-    ) -> Self {
-        Self {
-            status: status.into(),
-            old_eui64,
-            old_id,
-            old_extended_timeout,
-        }
-    }
-
-    pub fn status(&self) -> Result<Status, u8> {
-        Status::try_from(self.status)
-    }
-
+impl Payload {
     #[must_use]
     pub const fn old_eui64(&self) -> Eui64 {
         self.old_eui64
@@ -89,5 +58,26 @@ impl Response {
     #[must_use]
     pub const fn old_extended_timeout(&self) -> bool {
         self.old_extended_timeout
+    }
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, FromLeBytes)]
+pub struct Response {
+    status: u8,
+    payload: Payload,
+}
+
+impl Parameter for Response {
+    type Id = u16;
+    const ID: Self::Id = ID;
+}
+
+impl Resolve for Response {
+    type Result = Payload;
+
+    fn resolve(self) -> Result<Self::Result, Error> {
+        Status::try_from(self.status)
+            .resolve()
+            .map(|()| self.payload)
     }
 }

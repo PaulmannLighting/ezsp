1052705f94e0b59388d32a9af388062f2d65a904
Richard Neumann <neumann@paulmann.de>
2024-04-26 10:01:35 +0200

    Implement get_channel()

--- src/transport/ezsp/mfglib.rs
+++ src/transport/ezsp/mfglib.rs
@@ -1,7 +1,7 @@
 use std::future::Future;
 
 use crate::error::Resolve;
-use crate::frame::parameters::mfglib::end;
+use crate::frame::parameters::mfglib::{end, get_channel};
 use crate::{Error, Transport};
 
 pub trait Mfglib {
@@ -9,6 +9,9 @@ pub trait Mfglib {
     /// to [`start()`](Self::start) and stops receiving packets started by [`start()`](Self::start)
     /// at the same time.
     fn end(&self) -> impl Future<Output = Result<(), Error>> + Send;
+
+    /// Returns the current radio channel, as previously set via [`set_channel()`](Self::set_channel).
+    fn get_channel(&self) -> impl Future<Output = Result<u8, Error>> + Send;
 }
 
 impl<T> Mfglib for T
@@ -19,5 +22,11 @@ where
         self.communicate::<_, end::Response>(end::Command)
             .await?
             .resolve()
+    }
+
+    async fn get_channel(&self) -> Result<u8, Error> {
+        self.communicate::<_, get_channel::Response>(get_channel::Command)
+            .await
+            .map(|response| response.channel())
     }
 }

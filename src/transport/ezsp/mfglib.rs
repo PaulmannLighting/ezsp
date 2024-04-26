1567136b0e512844ac7fa3e9abba940e61bb47d4
Richard Neumann <neumann@paulmann.de>
2024-04-26 09:57:38 +0200

    Implement end()

--- src/transport/ezsp/mfglib.rs
+++ src/transport/ezsp/mfglib.rs
@@ -1,5 +1,22 @@
-use crate::Transport;
+use crate::error::Resolve;
+use crate::frame::parameters::mfglib::end;
+use crate::{Error, Transport};
+use std::future::Future;
 
-pub trait Mfglib {}
+pub trait Mfglib {
+    /// Deactivate use of `Mfglib` test routines; restores the hardware to the state it was in prior
+    /// to [`start()`](Self::start) and stops receiving packets started by [`start()`](Self::start)
+    /// at the same time.
+    fn end(&self) -> impl Future<Output = Result<(), Error>> + Send;
+}
 
-impl<T> Mfglib for T where T: Transport {}
+impl<T> Mfglib for T
+where
+    T: Transport,
+{
+    async fn end(&self) -> Result<(), Error> {
+        self.communicate::<_, end::Response>(end::Command)
+            .await?
+            .resolve()
+    }
+}

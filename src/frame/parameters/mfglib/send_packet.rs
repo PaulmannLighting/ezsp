31b4522ec42628eceafbe3fc90ba68462e7c8f8f
Richard Neumann <neumann@paulmann.de>
2024-04-26 10:27:04 +0200

    Implement send_packet()

--- src/frame/parameters/mfglib/send_packet.rs
+++ src/frame/parameters/mfglib/send_packet.rs
@@ -1,12 +1,21 @@
-use crate::ember::Status;
-use crate::types::ByteSizedVec;
 use le_stream::derive::{FromLeBytes, ToLeBytes};
+
+use crate::ember::Status;
+use crate::error::Resolve;
+use crate::frame::Parameter;
+use crate::types::ByteSizedVec;
+use crate::Error;
 
 const ID: u16 = 0x0089;
 
-#[derive(Debug, Eq, PartialEq, FromLeBytes, ToLeBytes)]
+#[derive(Debug, Eq, PartialEq, ToLeBytes)]
 pub struct Command {
     content: ByteSizedVec<u8>,
+}
+
+impl Parameter for Command {
+    type Id = u16;
+    const ID: Self::Id = ID;
 }
 
 impl Command {
@@ -14,14 +23,9 @@ impl Command {
     pub const fn new(content: ByteSizedVec<u8>) -> Self {
         Self { content }
     }
-
-    #[must_use]
-    pub const fn content(&self) -> &ByteSizedVec<u8> {
-        &self.content
-    }
 }
 
-#[derive(Debug, Eq, PartialEq, FromLeBytes, ToLeBytes)]
+#[derive(Clone, Debug, Eq, PartialEq, FromLeBytes)]
 pub struct Response {
     status: u8,
 }
@@ -33,8 +37,17 @@ impl Response {
             status: status.into(),
         }
     }
+}
 
-    pub fn status(&self) -> Result<Status, u8> {
-        Status::try_from(self.status)
+impl Parameter for Response {
+    type Id = u16;
+    const ID: Self::Id = ID;
+}
+
+impl Resolve for Response {
+    type Result = ();
+
+    fn resolve(self) -> Result<Self::Result, Error> {
+        Status::try_from(self.status).resolve()
     }
 }
--- src/transport/ezsp/mfglib.rs
+++ src/transport/ezsp/mfglib.rs
@@ -1,7 +1,8 @@
 use std::future::Future;
 
 use crate::error::Resolve;
-use crate::frame::parameters::mfglib::{end, get_channel, get_power};
+use crate::frame::parameters::mfglib::{end, get_channel, get_power, send_packet};
+use crate::types::ByteSizedVec;
 use crate::{Error, Transport};
 
 pub trait Mfglib {
@@ -15,6 +16,21 @@ pub trait Mfglib {
 
     /// Returns the current radio power setting, as previously set via [`set_power()`](Self::set_power).
     fn get_power(&self) -> impl Future<Output = Result<i8, Error>> + Send;
+
+    /// Sends a single packet consisting of the following bytes:
+    ///     * `packetLength`
+    ///     * `packetContents[0]`
+    ///     * ...
+    ///     * `packetContents[packetLength - 3]`
+    ///     * `CRC[0]`
+    ///     * `CRC[1]`
+    ///
+    /// The total number of bytes sent is packetLength + 1.
+    /// The radio replaces the last two bytes of `content` with the 16-bit CRC for the packet.
+    fn send_packet(
+        &self,
+        content: ByteSizedVec<u8>,
+    ) -> impl Future<Output = Result<(), Error>> + Send;
 }
 
 impl<T> Mfglib for T
@@ -37,5 +53,11 @@ where
         self.communicate::<_, get_power::Response>(get_power::Command)
             .await
             .map(|response| response.power())
+    }
+
+    async fn send_packet(&self, content: ByteSizedVec<u8>) -> Result<(), Error> {
+        self.communicate::<_, send_packet::Response>(send_packet::Command::new(content))
+            .await?
+            .resolve()
     }
 }

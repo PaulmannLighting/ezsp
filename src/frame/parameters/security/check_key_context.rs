9f16f4430a04ac27b7c8e145f2fe09b1838af6f9
Richard Neumann <neumann@paulmann.de>
2024-06-20 14:38:23 +0200

    Implement check_key_context()

--- src/frame/parameters/security/check_key_context.rs
+++ src/frame/parameters/security/check_key_context.rs
@@ -1,14 +1,23 @@
 use le_stream::derive::{FromLeBytes, ToLeBytes};
-
-use crate::frame::Parameter;
 use siliconlabs::zigbee::security::ManContext;
 use siliconlabs::Status;
+
+use crate::error::Resolve;
+use crate::frame::Parameter;
+use crate::Error;
 
 const ID: u16 = 0x0110;
 
 #[derive(Debug, Eq, PartialEq, ToLeBytes)]
 pub struct Command {
     context: ManContext,
+}
+
+impl Command {
+    #[must_use]
+    pub const fn new(context: ManContext) -> Self {
+        Self { context }
+    }
 }
 
 impl Parameter for Command {
@@ -21,13 +30,15 @@ pub struct Response {
     status: u32,
 }
 
-impl Response {
-    pub fn status(&self) -> Result<Status, u32> {
-        Status::try_from(self.status)
-    }
-}
-
 impl Parameter for Response {
     type Id = u16;
     const ID: Self::Id = ID;
+}
+
+impl Resolve for Response {
+    type Result = ();
+
+    fn resolve(self) -> Result<Self::Result, Error> {
+        Status::try_from(self.status).resolve()
+    }
 }
--- src/transport/ezsp/security.rs
+++ src/transport/ezsp/security.rs
@@ -1,10 +1,18 @@
 use std::future::Future;
 
+use siliconlabs;
+use siliconlabs::zigbee::security::ManContext;
+
 use crate::error::Resolve;
-use crate::frame::parameters::security::{clear_key_table, clear_transient_link_keys};
+use crate::frame::parameters::security::{
+    check_key_context, clear_key_table, clear_transient_link_keys,
+};
 use crate::{Error, Transport};
 
 pub trait Security {
+    /// Check whether a key context can be used to load a valid key.
+    fn check_key_context(&self, context: ManContext) -> impl Future<Output = Result<(), Error>>;
+
     /// This function clears the key table of the current network.
     fn clear_key_table(&self) -> impl Future<Output = Result<(), Error>>;
 
@@ -16,6 +24,12 @@ impl<T> Security for T
 where
     T: Transport,
 {
+    async fn check_key_context(&self, context: ManContext) -> Result<(), Error> {
+        self.communicate::<_, check_key_context::Response>(check_key_context::Command::new(context))
+            .await?
+            .resolve()
+    }
+
     async fn clear_key_table(&self) -> Result<(), Error> {
         self.communicate::<_, clear_key_table::Response>(clear_key_table::Command)
             .await?

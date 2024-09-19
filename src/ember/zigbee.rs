158f79c0fe83b5a85159867a91f659dbb189c1c8
Richard Neumann <neumann@paulmann.de>
2024-09-19 15:19:52 +0200

    Add docs for ember::zigbee::Network

--- src/ember/zigbee.rs
+++ src/ember/zigbee.rs
@@ -5,6 +5,7 @@ use std::array::IntoIter;
 use std::io::Read;
 use std::iter::{once, Chain, Once};
 
+/// The parameters of a ZigBee network.
 #[derive(Clone, Debug, Eq, PartialEq, FromLeBytes, ToLeBytes)]
 pub struct Network {
     channel: u8,
@@ -16,6 +17,7 @@ pub struct Network {
 }
 
 impl Network {
+    /// Create a new Zigbee network.
     #[must_use]
     pub const fn new(
         channel: u8,
@@ -35,31 +37,37 @@ impl Network {
         }
     }
 
+    /// Return the 802.15.4 channel associated with the network.
     #[must_use]
     pub const fn channel(&self) -> u8 {
         self.channel
     }
 
+    /// Return the network's PAN identifier.
     #[must_use]
     pub const fn pan_id(&self) -> PanId {
         self.pan_id
     }
 
+    /// Return the network's extended PAN identifier.
     #[must_use]
     pub const fn extended_pan_id(&self) -> u64 {
         self.extended_pan_id
     }
 
+    /// Return whether the network is allowing MAC associations.
     #[must_use]
     pub const fn allowing_join(&self) -> bool {
         self.allowing_join
     }
 
+    /// Return the Stack Profile associated with the network.
     #[must_use]
     pub const fn stack_profile(&self) -> u8 {
         self.stack_profile
     }
 
+    /// Return the instance of the Network.
     #[must_use]
     pub const fn nwk_update_id(&self) -> u8 {
         self.nwk_update_id

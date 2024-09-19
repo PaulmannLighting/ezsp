8e0dddf50e9a719184699d9c71a7d048af5b8018
Richard Neumann <neumann@paulmann.de>
2024-09-19 14:40:58 +0200

    Add docs for ember::radio::PowerMode

--- src/ember/radio.rs
+++ src/ember/radio.rs
@@ -1,10 +1,13 @@
 use num_derive::FromPrimitive;
 use num_traits::FromPrimitive;
 
+/// Ember radio power mode.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, Ord, PartialOrd, FromPrimitive)]
 #[repr(u8)]
 pub enum PowerMode {
+    /// The radio receiver is switched on.
     RxOn = 0x00,
+    /// The radio receiver is switched off.
     Off = 0x01,
 }
 

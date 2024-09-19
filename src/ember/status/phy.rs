c8ef0597ce16b3e5a4d9b9c7dcc2fd217295335f
Richard Neumann <neumann@paulmann.de>
2024-09-19 16:29:48 +0200

    Add docs for ember::status::phy::Phy

--- src/ember/status/phy.rs
+++ src/ember/status/phy.rs
@@ -3,16 +3,27 @@ use std::fmt::{Display, Formatter};
 
 use num_derive::FromPrimitive;
 
+/// Ember PHY status.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, Ord, PartialOrd, FromPrimitive)]
 #[repr(u8)]
 pub enum Phy {
+    /// The transmit hardware buffer underflowed.
     TxUnderflow = 0x88,
+    /// The transmit hardware did not finish transmitting a packet.
     TxIncomplete = 0x89,
+    /// An unsupported channel setting was specified.
     InvalidChannel = 0x8A,
+    /// An unsupported power setting was specified.
     InvalidPower = 0x8B,
+    /// The packet cannot be transmitted because the physical MAC layer is currently transmitting a packet.
+    ///
+    /// (This is used for the MAC backoff algorithm.)
     TxBusy = 0x8C,
+    /// The transmit attempt failed because all CCA attempts indicated that the channel was busy.
     TxCcaFail = 0x8D,
+    /// The software installed on the hardware doesn't recognize the hardware radio type.
     OscillatorCheckFailed = 0x8E,
+    /// The expected ACK was received after the last transmission.
     AckReceived = 0x8F,
 }
 

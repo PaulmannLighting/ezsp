1bf8fd14bdb51cfff285da535c01525a67431439
Richard Neumann <neumann@paulmann.de>
2024-09-19 16:31:37 +0200

    Add docs for ember::status::serial::Serial

--- src/ember/status/serial.rs
+++ src/ember/status/serial.rs
@@ -3,16 +3,25 @@ use std::fmt::{Display, Formatter};
 
 use num_derive::FromPrimitive;
 
+/// Ember serial status.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, Ord, PartialOrd, FromPrimitive)]
 #[repr(u8)]
 pub enum Serial {
+    /// Specified an invalid baud rate.
     InvalidBaudRate = 0x20,
+    /// Specified an invalid serial port.
     InvalidPort = 0x21,
+    /// Tried to send too much data.
     TxOverflow = 0x22,
+    /// There was not enough space to store a received character and the character was dropped.
     RxOverflow = 0x23,
+    /// Detected a UART framing error.
     RxFrameError = 0x24,
+    /// Detected a UART parity error.
     RxParityError = 0x025,
+    /// There is no received data to process.
     RxEmpty = 0x26,
+    /// The receive interrupt was not handled in time, and a character was dropped.
     RxOverrunError = 0x27,
 }
 

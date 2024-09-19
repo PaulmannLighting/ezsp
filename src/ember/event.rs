dee9a50fa1e67883ff01534bcfc20c85fc45f2ba
Richard Neumann <neumann@paulmann.de>
2024-09-19 14:05:57 +0200

    Add docs for ember::event::Units

--- src/ember/event.rs
+++ src/ember/event.rs
@@ -1,12 +1,17 @@
 use num_derive::FromPrimitive;
 use num_traits::FromPrimitive;
 
+/// Ember event units.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, Ord, PartialOrd, FromPrimitive)]
 #[repr(u8)]
 pub enum Units {
+    /// The event is not scheduled to run.
     Inactive = 0x00,
+    /// The execution time is in approximate milliseconds.
     MsTime = 0x01,
+    /// The execution time is in 'binary' quarter seconds (256 approximate milliseconds each).
     QsTime = 0x02,
+    /// The execution time is in 'binary' minutes (65536 approximate milliseconds each).
     MinuteTime = 0x03,
 }
 

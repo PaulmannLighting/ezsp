e33fba2ab280859a580407d33e061805fc674edf
Richard Neumann <neumann@paulmann.de>
2024-09-19 11:11:09 +0200

    Add type docs

--- src/types.rs
+++ src/types.rs
@@ -1,11 +1,21 @@
+/// A heap-allocated vector with a maximum size of 255 elements.
 pub type ByteSizedVec<T> = heapless::Vec<T, { u8::MAX as usize }>;
+/// A type alias for a 16-bit unsigned integer.
+///
+/// See p. 129, `gpSinkTableGetNumberOfActiveEntries` of the EZSP specification.
 pub type UintT = u16; // TODO: is this the correct type?
 
+/// Discovery mode for source routes.
+///
+/// See p. 88 `setSourceRouteDiscoveryMode` of the EZSP specification.
 #[derive(Clone, Debug, Eq, PartialEq)]
 #[repr(u8)]
 pub enum SourceRouteDiscoveryMode {
+    /// Source route discovery is off.
     Off = 0,
+    /// Source route discovery is on.
     On = 1,
+    /// Source route discovery is set to reschedule.
     Reschedule = 2,
 }
 

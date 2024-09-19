0edc41ae3cc3beff08b0a696fd66ed8356cf8813
Richard Neumann <neumann@paulmann.de>
2024-09-19 15:22:18 +0200

    Add docs for ember::gp::address::Address

--- src/ember/gp/address.rs
+++ src/ember/gp/address.rs
@@ -1,5 +1,6 @@
 use le_stream::derive::{FromLeBytes, ToLeBytes};
 
+/// A GP address structure.
 #[derive(Clone, Debug, Eq, PartialEq, FromLeBytes, ToLeBytes)]
 pub struct Address {
     id: [u8; 8],
@@ -8,6 +9,7 @@ pub struct Address {
 }
 
 impl Address {
+    /// Create a new GP address.
     #[must_use]
     pub const fn new(id: [u8; 8], application_id: u8, endpoint: u8) -> Self {
         Self {
@@ -17,16 +19,20 @@ impl Address {
         }
     }
 
+    /// Return what either contains a 4-byte source ID or an 8-byte IEEE address,
+    /// as indicated by the value of the applicationId field.
     #[must_use]
     pub const fn id(&self) -> &[u8; 8] {
         &self.id
     }
 
+    /// Return the GPD Application ID specifying either source ID (`0x00`) or IEEE address (`0x02`).
     #[must_use]
     pub const fn application_id(&self) -> u8 {
         self.application_id
     }
 
+    /// Return the GPD endpoint.
     #[must_use]
     pub const fn endpoint(&self) -> u8 {
         self.endpoint

1f7ae6f706a77d5b87b022c5fe8d6d7443481a2d
Richard Neumann <neumann@paulmann.de>
2024-09-19 15:10:38 +0200

    Add docs for ember::status::Status

--- src/ember/status.rs
+++ src/ember/status.rs
@@ -23,66 +23,166 @@ mod phy;
 mod serial;
 mod sim_eeprom;
 
+/// Ember status.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, Ord, PartialOrd)]
 #[repr(u8)]
 pub enum Status {
+    /// The generic 'no error' message.
     Success = 0x00,
+    /// The generic 'fatal error' message.
     ErrFatal = 0x01,
+    /// An invalid value was passed as an argument to a function.
     BadArgument = 0x02,
+    /// EEPROM status.
     Eeprom(Eeprom),
+    /// There are no more buffers.
     NoBuffers = 0x18,
+    /// Serial status.
     Serial(Serial),
+    /// MAC status.
     Mac(Mac),
+    /// Simulated EEPROM status.
     SimEeprom(SimEeprom),
+    /// Erroneous messages.
     Err(err::Err),
+    /// The APS layer attempted to send or deliver a message, but it failed.
     DeliveryFailed = 0x66,
+    /// This binding index is out of range of the current binding table.
     BindingIndexOutOfRange = 0x69,
+    /// This address table index is out of range for the current address table.
     AddressTableIndexOutOfRange = 0x6A,
+    /// An invalid binding table index was given to a function.
     InvalidBindingIndex = 0x6C,
+    /// The API call is not allowed given the current state of the stack.
     InvalidCall = 0x70,
+    /// The link cost to a node is not known.
     CostNotKnown = 0x71,
+    /// The maximum number of in-flight messages (i.e. EMBER_APS_UNICAST_MESSAGE_COUNT) has been reached.
     MaxMessageLimitReached = 0x72,
+    /// The message to be transmitted is too big to fit into a single over-the-air packet.
     MessageTooLong = 0x74,
+    /// The application is trying to delete or overwrite a binding that is in use.
     BindingIsActive = 0x75,
+    /// The application is trying to overwrite an address table entry that is in use.
     AddressTableEntryIsActive = 0x76,
+    /// ADC status.
     Adc(Adc),
+    /// Sleeping (for a duration) has been abnormally interrupted and exited prematurely.
     SleepInterrupted = 0x85,
+    /// PHY status.
     Phy(Phy),
+    /// The stack software has completed initialization and is ready to send and receive packets over the air.
     NetworkUp = 0x90,
+    /// The network is not operating.
     NetworkDown = 0x91,
+    /// The node has not joined a network.
     NotJoined = 0x93,
+    /// An attempt to join a network failed.
     JoinFailed = 0x94,
+    /// The chosen security level (the value of `EMBER_SECURITY_LEVEL`) is not supported by the stack.
     InvalidSecurityLevel = 0x95,
+    /// After moving, a mobile node's attempt to re-establish contact with the network failed.
     MoveFailed = 0x96,
+    /// An attempt to join as a router failed due to a ZigBee versus ZigBee Pro incompatibility.
+    ///
+    /// ZigBee devices joining ZigBee Pro networks (or vice versa) must join as End Devices, not Routers.
     CannotJoinAsRouter = 0x98,
+    /// The local node ID has changed.
+    ///
+    /// The application can obtain the new node ID by calling `emberGetNodeId()`.
     NodeIdChanged = 0x99,
+    /// The local PAN ID has changed.
+    ///
+    /// The application can obtain the new PAN ID by calling `emberGetPanId()`.
     PanIdChanged = 0x9A,
+    /// The network has been opened for joining.
     NetworkOpened = 0x9C,
+    /// The network has been closed for joining.
     NetworkClosed = 0x9D,
+    /// An attempt to join or rejoin the network failed because
+    /// no router beacons could be heard by the joining node.
     NoBeacons = 0xAB,
+    /// An attempt was made to join a Secured Network using a pre-configured key,
+    /// but the Trust Center sent back a Network Key in-the-clear when an encrypted
+    /// Network Key was required.
     ReceivedKeyInTheClear = 0xAC,
+    /// An attempt was made to join a Secured Network, but the device did not receive a Network Key.
     NoNetworkKeyReceived = 0xAD,
+    /// After a device joined a Secured Network, a Link Key was requested but no response was ever received.
     NoLinkKeyReceived = 0xAE,
+    /// An attempt was made to join a Secured Network without a pre-configured key,
+    /// but the Trust Center sent encrypted data using a pre-configured key.
     PreconfiguredKeyRequired = 0xAF,
+    /// A message cannot be sent because the network is currently overloaded.
     NetworkBusy = 0xA1,
+    /// The application tried to send a message using an endpoint that it has not defined.
     InvalidEndpoint = 0xA3,
+    /// The application tried to use a binding that has been remotely modified
+    /// and the change has not yet been reported to the application.
     BindingHasChanged = 0xA4,
+    /// An attempt to generate random bytes failed because of insufficient random data from the radio.
     InsufficientRandomData = 0xA5,
+    /// There was an error in trying to encrypt at the APS  Level.
+    ///
+    /// This could result from either an inability to determine the long address of the
+    /// recipient from the short address (no entry in the binding table) or there is no
+    /// link key entry in the table associated with the destination, or there was a failure
+    /// to load the correct key into the encryption core.
     ApsEncryptionError = 0xA6,
+    /// There was an attempt to form or join a network with security
+    /// without calling `emberSetInitialSecurityState()` first.
     SecurityStateNotSet = 0xA8,
+    /// A ZigBee route error command frame was received indicating that
+    /// a source routed message from this node failed en route
     SourceRouteFailure = 0xA9,
+    /// A ZigBee route error command frame was received indicating that a message sent
+    /// to this node along a many-to-one route failed en route.
+    ///
+    /// The route error frame was delivered by an ad-hoc search for a functioning route.
     ManyToOneRouteFailure = 0xAA,
+    /// A critical and fatal error indicating that the version of the stack trying
+    /// to run does not match with the chip it is running on.
+    ///
+    /// The software (stack) on the chip must be replaced with software
+    /// that is compatible with the chip.
     StackAndHardwareMismatch = 0xB0,
+    /// An index was passed into the function that was larger than the valid range.
     IndexOutOfRange = 0xB1,
+    /// There was an attempt to set an entry in the key table  using an invalid long address.
+    ///
+    /// An entry cannot be set using either the local device's or Trust Center's IEEE address.
+    /// Or an entry already exists in the table with the same IEEE address.
+    /// An Address of all zeros or  all F's are not valid addresses in 802.15.4.
     KeyTableInvalidAddress = 0xB3,
+    /// There are no empty entries left in the table.
     TableFull = 0xB4,
+    /// The requested function cannot be executed because the library that contains
+    /// the necessary functionality is not present.
     LibraryNotPresent = 0xB5,
+    /// The requested table entry has been erased and contains no valid data.
     TableEntryErased = 0xB6,
+    /// There was an attempt to set a security configuration that is not valid
+    /// given the other security settings.
     SecurityConfigurationInvalid = 0xB7,
+    /// There was an attempt to broadcast a key switch too quickly after broadcasting the next network key.
+    ///
+    /// The Trust Center must wait at least a period equal to the broadcast timeout
+    /// so that all routers have a chance to receive the broadcast of the new network key.
     TooSoonForSwitchKey = 0xB8,
+    /// The stack accepted the command and is currently processing the request.
+    ///
+    /// The results will be returned via an appropriate handler.
     OperationInProgress = 0xBA,
+    /// The message could not be sent because the link key corresponding to the destination
+    /// is not authorized for use in APS data messages.
+    ///
+    /// APS Commands (sent by the stack) are allowed.
+    /// To use it for encryption of APS data messages it must be authorized using
+    /// a key agreement protocol (such as CBKE).
     KeyNotAuthorized = 0xBB,
+    /// The security data provided was not valid, or an integrity check failed.
     SecurityDataInvalid = 0xBD,
+    /// Application status.
     Application(Application),
 }
 

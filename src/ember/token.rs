e27864c1405f4cc3b27ce8e01ee7a6086b3030c9
Richard Neumann <neumann@paulmann.de>
2024-09-19 15:14:25 +0200

    Add docs for ember::token::Info

--- src/ember/token.rs
+++ src/ember/token.rs
@@ -27,6 +27,7 @@ impl Data {
     }
 }
 
+/// Information of a token in the token table.
 #[derive(Clone, Debug, Eq, PartialEq, FromLeBytes, ToLeBytes)]
 pub struct Info {
     nvm3_key: u32,
@@ -37,6 +38,7 @@ pub struct Info {
 }
 
 impl Info {
+    /// Create new token info.
     #[must_use]
     pub const fn new(nvm3_key: u32, is_cnt: bool, is_idx: bool, size: u8, array_size: u8) -> Self {
         Self {
@@ -48,26 +50,31 @@ impl Info {
         }
     }
 
+    /// Return NVM3 key of the token.
     #[must_use]
     pub const fn nvm3_key(&self) -> u32 {
         self.nvm3_key
     }
 
+    /// Return whether token is a counter type.
     #[must_use]
     pub const fn is_cnt(&self) -> bool {
         self.is_cnt
     }
 
+    /// Return whether token is an indexed token.
     #[must_use]
     pub const fn is_idx(&self) -> bool {
         self.is_idx
     }
 
+    /// Return size of the token.
     #[must_use]
     pub const fn size(&self) -> u8 {
         self.size
     }
 
+    /// Return array size of the token.
     #[must_use]
     pub const fn array_size(&self) -> u8 {
         self.array_size

c2e0b7417a2c0de96353343b1ab118dcd6f84a02
Richard Neumann <neumann@paulmann.de>
2024-09-19 14:40:24 +0200

    Add docs for ember::node::Type

--- src/ember/node.rs
+++ src/ember/node.rs
@@ -1,13 +1,21 @@
 use num_derive::FromPrimitive;
 use num_traits::FromPrimitive;
 
+/// Ember node type.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, Ord, PartialOrd, FromPrimitive)]
 #[repr(u8)]
 pub enum Type {
+    /// Device is not joined.
     UnknownDevice = 0x00,
+    /// Will relay messages and can act as a parent to other nodes.
     Coordinator = 0x01,
+    /// Will relay messages and can act as a parent to other nodes.
     Router = 0x02,
+    /// Communicates only with its parent and will not relay messages.
     EndDevice = 0x03,
+    /// An end device whose radio can be turned off to save power.
+    ///
+    /// The application must poll to receive messages.
     SleepyEndDevice = 0x04,
 }
 

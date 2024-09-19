bc4b040a1e38fca6237ee0b6c53b31df1123ebff
Richard Neumann <neumann@paulmann.de>
2024-09-19 14:31:21 +0200

    Add docs for ember::message::Incoming

--- src/ember/message.rs
+++ src/ember/message.rs
@@ -1,15 +1,23 @@
 use num_derive::FromPrimitive;
 use num_traits::FromPrimitive;
 
+/// Ember incoming message type.
 #[derive(Debug, Clone, Copy, Ord, PartialOrd, Eq, PartialEq, FromPrimitive)]
 #[repr(u8)]
 pub enum Incoming {
+    /// Unicast.
     Unicast = 0x00,
+    /// Unicast reply.
     UnicastReply = 0x01,
+    /// Multicast.
     Multicast = 0x02,
+    /// Multicast sent by the local device.
     MulticastLoopback = 0x03,
+    /// Broadcast.
     Broadcast = 0x04,
+    /// Broadcast sent by the local device.
     BroadcastLoopback = 0x05,
+    /// Many to one route request.
     ManyToOneRouteRequest = 0x06,
 }
 

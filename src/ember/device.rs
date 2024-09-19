e7533bb94e3ebec681a539cd1b3156099ff73004
Richard Neumann <neumann@paulmann.de>
2024-09-19 13:23:19 +0200

    Add docs for ember::device::Update

--- src/ember/device.rs
+++ src/ember/device.rs
@@ -1,12 +1,17 @@
 use num_derive::FromPrimitive;
 use num_traits::FromPrimitive;
 
+/// Ember device update.
 #[derive(Debug, Clone, Copy, Ord, PartialOrd, Eq, PartialEq, FromPrimitive)]
 #[repr(u8)]
 pub enum Update {
+    /// A secured rejoin occurred.
     StandardSecuritySecuredRejoin = 0x00,
+    /// An unsecured join occurred.
     StandardSecurityUnsecuredJoin = 0x01,
+    /// A device left the network.
     DeviceLeft = 0x02,
+    /// An unsecured rejoin occurred.
     StandardSecurityUnsecuredRejoin = 0x03,
 }
 

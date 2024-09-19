cb403785286daaa41da90dfe5481b9ff4b52c352
Richard Neumann <neumann@paulmann.de>
2024-09-19 16:58:02 +0200

    Add docs for ember::zll::state::State

--- src/ember/zll/state.rs
+++ src/ember/zll/state.rs
@@ -1,14 +1,21 @@
 use num_derive::FromPrimitive;
 use num_traits::FromPrimitive;
 
+/// Ember ZLL state.
 #[derive(Debug, Clone, Copy, Ord, PartialOrd, Eq, PartialEq, FromPrimitive)]
 #[repr(u16)]
 pub enum State {
+    /// No state.
     None = 0x0000,
+    /// The device is factory new.
     FactoryNew = 0x0001,
+    /// The device is capable of assigning addresses to other devices.
     AddressAssignmentCapable = 0x0002,
+    /// The device is initiating a link operation.
     LinkInitiator = 0x0010,
+    /// The device is requesting link priority.
     LinkPriorityRequest = 0x0020,
+    /// The device is on a non-ZLL network.
     NonZllNetwork = 0x0100,
 }
 

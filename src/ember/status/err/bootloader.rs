783f1284d5df2b24a98ef2759d3ca9f1815bc98c
Richard Neumann <neumann@paulmann.de>
2024-09-19 16:36:10 +0200

    Add docs for ember::status::err::bootloader::Bootloader

--- src/ember/status/err/bootloader.rs
+++ src/ember/status/err/bootloader.rs
@@ -3,11 +3,16 @@ use std::fmt::{Display, Formatter};
 
 use num_derive::FromPrimitive;
 
+/// Ember bootloader status.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, Ord, PartialOrd, FromPrimitive)]
 #[repr(u8)]
 pub enum Bootloader {
+    /// The bootloader received an invalid message (failed attempt to go into bootloader).
     TrapTableBad = 0x58,
+    /// Bootloader received an invalid message (failed attempt to go into bootloader).
     TrapUnknown = 0x59,
+    /// The bootloader cannot complete the bootload operation because either an image was not found
+    /// or the image exceeded memory bounds.
     NoImage = 0x5A,
 }
 

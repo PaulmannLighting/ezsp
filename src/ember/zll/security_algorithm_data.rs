67394e37ea8a189d5475ebffad16a83fddbf10a9
Richard Neumann <neumann@paulmann.de>
2024-09-19 16:53:22 +0200

    Add docs for ember::zll::security_algorithm_data::SecurityAlgorithmData

--- src/ember/zll/security_algorithm_data.rs
+++ src/ember/zll/security_algorithm_data.rs
@@ -1,5 +1,6 @@
 use le_stream::derive::{FromLeBytes, ToLeBytes};
 
+/// Data associated with the ZLL security algorithm.
 #[derive(Clone, Debug, Eq, PartialEq, FromLeBytes, ToLeBytes)]
 pub struct SecurityAlgorithmData {
     transaction_id: u32,
@@ -8,6 +9,7 @@ pub struct SecurityAlgorithmData {
 }
 
 impl SecurityAlgorithmData {
+    /// Create new ZLL security algorithm data.
     #[must_use]
     pub const fn new(transaction_id: u32, response_id: u32, bitmask: u16) -> Self {
         Self {
@@ -17,16 +19,19 @@ impl SecurityAlgorithmData {
         }
     }
 
+    /// Return the transaction identifier.
     #[must_use]
     pub const fn transaction_id(&self) -> u32 {
         self.transaction_id
     }
 
+    /// Return the response identifier.
     #[must_use]
     pub const fn response_id(&self) -> u32 {
         self.response_id
     }
 
+    /// Return the bitmask.
     #[must_use]
     pub const fn bitmask(&self) -> u16 {
         self.bitmask

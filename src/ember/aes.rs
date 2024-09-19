f6cbffefe31e78b9cde68482ca1fad2fe5c29bc5
Richard Neumann <neumann@paulmann.de>
2024-09-19 11:34:58 +0200

    Add aes docs

--- src/ember/aes.rs
+++ src/ember/aes.rs
@@ -1,5 +1,6 @@
 use le_stream::derive::{FromLeBytes, ToLeBytes};
 
+/// The hash context for an ongoing hash operation.
 #[derive(Clone, Debug, Eq, PartialEq, FromLeBytes, ToLeBytes)]
 pub struct MmoHashContext {
     result: [u8; 16],
@@ -7,16 +8,19 @@ pub struct MmoHashContext {
 }
 
 impl MmoHashContext {
+    /// Create a new hash context.
     #[must_use]
     pub const fn new(result: [u8; 16], length: u32) -> Self {
         Self { result, length }
     }
 
+    /// Return the result of ongoing the hash operation.
     #[must_use]
     pub const fn result(&self) -> &[u8; 16] {
         &self.result
     }
 
+    /// Return the total length of the data that has been hashed so far.
     #[must_use]
     pub const fn length(&self) -> u32 {
         self.length

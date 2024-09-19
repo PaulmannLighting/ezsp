687a78a0bafc9c4bb7eac1cfb1d79c317a040d9c
Richard Neumann <neumann@paulmann.de>
2024-09-19 16:24:25 +0200

    Add docs for ember::status::eeprom::Eeprom

--- src/ember/status/eeprom.rs
+++ src/ember/status/eeprom.rs
@@ -3,11 +3,18 @@ use std::fmt::{Display, Formatter};
 
 use num_derive::FromPrimitive;
 
+/// Ember EEPROM status.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, Ord, PartialOrd, FromPrimitive)]
 #[repr(u8)]
 pub enum Eeprom {
+    /// The manufacturing and stack token format in non-volatile memory is different
+    /// from what the stack expects (returned at initialization).
     MfgStackVersionMismatch = 0x04,
+    /// The manufacturing token format in non-volatile memory is different
+    /// from what the stack expects (returned at initialization).
     MfgVersionMismatch = 0x06,
+    /// The stack token format in non-volatile memory is different
+    /// from what the stack expects (returned at initialization).
     StackVersionMismatch = 0x07,
 }
 

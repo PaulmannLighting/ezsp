4e5ccfdd41de67b1299b4f849ad58f1c75a77ecd
Richard Neumann <neumann@paulmann.de>
2024-09-19 15:25:10 +0200

    Add docs for ember::gp::proxy::TableEntry

--- src/ember/gp/proxy.rs
+++ src/ember/gp/proxy.rs
@@ -8,6 +8,7 @@ use crate::ember::NodeId;
 
 type Status = u8;
 
+/// The internal representation of a proxy table entry.
 #[derive(Clone, Debug, Eq, PartialEq, FromLeBytes, ToLeBytes)]
 pub struct TableEntry {
     status: Status,
@@ -23,6 +24,7 @@ pub struct TableEntry {
 }
 
 impl TableEntry {
+    /// Create a new proxy table entry.
     #[allow(clippy::too_many_arguments)]
     #[must_use]
     pub const fn new(
@@ -51,51 +53,61 @@ impl TableEntry {
         }
     }
 
+    /// Return the internal status of the proxy table entry.
     #[must_use]
     pub const fn status(&self) -> Status {
         self.status
     }
 
+    /// Return the tunneling options (this contains both options and extendedOptions from the spec).
     #[must_use]
     pub const fn options(&self) -> u32 {
         self.options
     }
 
+    /// Return the addressing info of the GPD.
     #[must_use]
     pub const fn gpd(&self) -> &Address {
         &self.gpd
     }
 
+    /// Return the assigned alias for the GPD.
     #[must_use]
     pub const fn assigned_alias(&self) -> NodeId {
         self.assigned_alias
     }
 
+    /// Return the security options field.
     #[must_use]
     pub const fn security_options(&self) -> u8 {
         self.security_options
     }
 
+    /// Return the security frame counter of the GPD.
     #[must_use]
     pub const fn gpd_security_frame_counter(&self) -> FrameCounter {
         self.gpd_security_frame_counter
     }
 
+    /// Return the key to use for GPD.
     #[must_use]
     pub const fn gpd_key(&self) -> &Data {
         &self.gpd_key
     }
 
+    /// Return the list of sinks (hardcoded to 2 which is the spec minimum).
     #[must_use]
     pub const fn sink_list(&self) -> &[ListEntry; LIST_ENTRIES] {
         &self.sink_list
     }
 
+    /// Return the group cast radius.
     #[must_use]
     pub const fn group_cast_radius(&self) -> u8 {
         self.group_cast_radius
     }
 
+    /// Return the search counter.
     #[must_use]
     pub const fn search_counter(&self) -> u8 {
         self.search_counter

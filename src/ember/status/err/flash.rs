2506e8a6b9bee4fdfafeb77933c1e79a9579d553
Richard Neumann <neumann@paulmann.de>
2024-09-19 16:38:33 +0200

    Add docs for ember::status::err::flash::Flash

--- src/ember/status/err/flash.rs
+++ src/ember/status/err/flash.rs
@@ -3,12 +3,33 @@ use std::fmt::{Display, Formatter};
 
 use num_derive::FromPrimitive;
 
+/// Ember flash error status.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, Ord, PartialOrd, FromPrimitive)]
 #[repr(u8)]
 pub enum Flash {
+    /// A fatal error has occurred while trying to write data to the Flash.
+    ///
+    /// The target memory attempting to be programmed is already programmed.
+    /// The flash write routines were asked to flip a bit from a 0 to 1,
+    /// which is physically impossible and the write was therefore inhibited.
+    /// The data in the flash cannot be trusted after this error.
     WriteInhibited = 0x46,
+    /// A fatal error has occurred while trying to write data to the Flash
+    /// and the write verification has failed.
+    ///
+    /// The data in the flash cannot be trusted after this error,
+    /// and it is possible this error is the result of exceeding the life cycles of the flash.
     VerifyFailed = 0x47,
+    /// A fatal error has occurred while trying to write data to the flash,
+    /// possibly due to write protection or an invalid address.
+    ///
+    /// The data in the flash cannot be trusted after this error,
+    /// and it is possible this error is the result of exceeding the life cycles of the flash.
     ProgFail = 0x4B,
+    /// A fatal error has occurred while trying to erase flash, possibly due to write protection.
+    ///
+    /// The data in the flash cannot be trusted after this error,
+    /// and it is possible this error is the result of exceeding the life cycles of the flash.
     EraseFail = 0x4C,
 }
 

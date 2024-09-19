acd5c3492f6553fafd6dd56f835bfee301c4fc81
Richard Neumann <neumann@paulmann.de>
2024-09-19 16:16:56 +0200

    Add docs for ember::security::initial::Bitmask

--- src/ember/security/initial.rs
+++ src/ember/security/initial.rs
@@ -5,20 +5,70 @@ use num_traits::FromPrimitive;
 use crate::ember::key::Data;
 use crate::ember::Eui64;
 
+/// Ember initial security bitmask.
 #[derive(Debug, Clone, Copy, Ord, PartialOrd, Eq, PartialEq, FromPrimitive)]
 #[repr(u16)]
 pub enum Bitmask {
+    /// This enables ZigBee Standard Security on the node.
     StandardSecurityMode = 0x0000,
+    /// This enables Distributed Trust Center Mode for the device forming the network.
+    /// (Previously known as `EMBER_NO_TRUST_CENTER_MODE`)
     DistributedTrustCenterMode = 0x0002,
+    /// This enables a Global Link Key for the Trust Center.
+    /// All nodes will share the same Trust Center Link Key.
     TrustCenterGlobalLinkKey = 0x0004,
+    /// This enables devices that perform MAC Association with a pre-configured
+    /// Network Key to join the network.
+    /// It is only set on the Trust Center.
     PreconfiguredNetworkKeyMode = 0x0008,
+    /// This denotes that the preconfiguredKey is not the actual Link Key
+    /// but a Secret Key known only to the Trust Center.
+    ///
+    /// It is hashed with the IEEE Address of the destination device in order to
+    /// create the actual Link Key used in encryption.
+    /// This bit is only used by the Trust Center.
+    /// The joining device need not set this.
     TrustCenterUsesHashedLinkKey = 0x0084,
+    /// This denotes that the preconfiguredKey element has valid data that should
+    /// be used to configure the initial security state.
     HavePreconfiguredKey = 0x0100,
+    /// This denotes that the networkKey element has valid data that should
+    /// be used to configure the initial security state.
     HaveNetworkKey = 0x0200,
+    /// This denotes to a joining node that it should attempt to acquire a Trust Center Link Key
+    /// during joining.
+    ///
+    /// This is only necessary if the device does not have a pre-configured key.
     GetLinkKeyWhenJoining = 0x0400,
+    /// This denotes that a joining device should only accept an encrypted network key
+    /// from the Trust Center (using its pre-configured key).
+    ///
+    /// A key sent in-the-clear by the Trust Center will be rejected and the join will fail.
+    /// This option is only valid when utilizing a pre-configured key.
     RequireEncryptedKey = 0x0800,
+    /// This denotes whether the device should NOT reset its  outgoing frame counters
+    /// (both NWK and APS) when `emberSetInitialSecurityState()` is called.
+    ///
+    /// Normally it is advised to reset the frame counter before joining a new network.
+    /// However, in cases where a device is joining to the same network again
+    /// (but not using `emberRejoinNetwork()`) it should keep the NWK and APS frame counters
+    /// stored in its tokens.
     NoFrameCounterReset = 0x1000,
+    /// This denotes that the device should obtain its preconfigured key from an installation code
+    /// stored in the manufacturing token.
+    ///
+    /// The token contains a value that will be hashed to obtain the actual preconfigured key.
+    /// If that token is not valid, then the call to `emberSetInitialSecurityState()` will fail.
     GetPreconfiguredKeyFromInstallCode = 0x2000,
+    /// This denotes that the `EmberInitialSecurityState::preconfiguredTrustCenterEui64`
+    /// has a value in it containing the trust center EUI64.
+    ///
+    /// The device will only join a network and accept commands from a trust center with that EUI64.
+    /// Normally this bit is NOT set, and the EUI64 of the trust center is learned during the join process.
+    /// When commissioning a device to join onto an existing network, which is using a trust center,
+    /// and without sending any messages, this bit must be set and the field
+    /// `EmberInitialSecurityState::preconfiguredTrustCenterEui64` must be populated with the
+    /// appropriate EUI64.
     HaveTrustCenterEui64 = 0x0040,
 }
 

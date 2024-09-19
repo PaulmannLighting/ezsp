665f16b4f6c96138c2ce81a47bbcb4f35737abc3
Richard Neumann <neumann@paulmann.de>
2024-09-19 13:51:13 +0200

    Add docs for ember::entropy::Source

--- src/ember/entropy.rs
+++ src/ember/entropy.rs
@@ -1,12 +1,17 @@
 use num_derive::FromPrimitive;
 use num_traits::FromPrimitive;
 
+/// Ember entropy source.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, Ord, PartialOrd, FromPrimitive)]
 #[repr(u8)]
 pub enum Source {
+    /// Entropy source error.
     Error = 0,
+    /// Entropy source is the radio.
     Radio = 1,
+    /// Entropy source is the TRNG powered by mbed TLS.
     MbedTlsTRNG = 2,
+    /// Entropy source is powered by mbed TLS, the source is not TRNG.
     MbedTls = 3,
 }
 

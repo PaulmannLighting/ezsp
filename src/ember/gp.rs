c8876b52a50609e9bf37476c7663a36fef4596c0
Richard Neumann <neumann@paulmann.de>
2024-09-19 14:08:15 +0200

    Add docs for ember::gp::* type aliases

--- src/ember/gp.rs
+++ src/ember/gp.rs
@@ -5,6 +5,11 @@ pub mod sink;
 
 pub use address::Address;
 
+/// Type alias for the GP security level.
 pub type SecurityLevel = u8;
+
+/// Type alias for the GP key type.
 pub type KeyType = u8;
+
+/// Type alias for the GP sink table entry status.
 pub type SinkTableEntryStatus = u8;

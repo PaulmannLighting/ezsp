6de211062ebb0e4163dfef68c66c33a287e60e1d
Richard Neumann <neumann@paulmann.de>
2024-09-19 16:06:19 +0200

    Add docs for ember::network::status::Status

--- src/ember/network/status.rs
+++ src/ember/network/status.rs
@@ -4,13 +4,19 @@ use num_traits::FromPrimitive;
 use crate::Error;
 use crate::Resolve;
 
+/// Ember network status.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, Ord, PartialOrd, FromPrimitive)]
 #[repr(u8)]
 pub enum Status {
+    /// The node is not associated with a network in any way.
     NoNetwork = 0x00,
+    /// The node is currently attempting to join a network.
     JoiningNetwork = 0x01,
+    /// The node is joined to a network.
     JoinedNetwork = 0x02,
+    /// The node is an end device joined to a network but its parent is not responding.
     JoinedNetworkNoParent = 0x03,
+    /// The node is in the process of leaving its current network.
     LeavingNetwork = 0x04,
 }
 

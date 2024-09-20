9f3ddb94b1ae16258eaf9e52bff1266b9055809c
Richard Neumann <neumann@paulmann.de>
2024-09-20 16:01:08 +0200

    Update docs for ezsp::config::Id

--- src/ezsp/config.rs
+++ src/ezsp/config.rs
@@ -1,7 +1,7 @@
 use num_derive::FromPrimitive;
 use num_traits::FromPrimitive;
 
-/// EZSP configuration identifier.
+/// Identifies a configuration value.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, Ord, PartialOrd, FromPrimitive)]
 #[repr(u8)]
 pub enum Id {

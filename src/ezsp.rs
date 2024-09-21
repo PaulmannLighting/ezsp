6da2972a2c668b738f3f46f0257d91fff695866b
Richard Neumann <neumann@paulmann.de>
2024-09-21 15:35:15 +0200

    Remove obsolete types

--- src/ezsp.rs
+++ src/ezsp.rs
@@ -5,10 +5,8 @@ pub mod policy;
 pub mod config;
 pub mod decision;
 mod status;
-mod types;
 pub mod value;
 mod zll;
 
 pub use status::{Ash, Error, SpiErr, Status};
-pub use types::{SecureRandomNumber, SecureSessionId, SecurityLevel, SecurityType};
 pub use zll::NetworkOperation;
--- src/ezsp/types.rs
+++
@@ -1,4 +0,0 @@
-pub type SecurityType = u32;
-pub type SecurityLevel = u32;
-pub type SecureRandomNumber = [u8; 16];
-pub type SecureSessionId = [u8; 8];

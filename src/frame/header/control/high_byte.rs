4ffb1ef44e4557a1f3b34336c79ec1ee403bd054
Richard Neumann <neumann@paulmann.de>
2024-10-11 16:45:14 +0200

    [WIP] Reimplement header control bytes with bitflags

--- src/frame/header/control/high_byte.rs
+++ src/frame/header/control/high_byte.rs
@@ -1,92 +1,13 @@
-use le_stream::derive::{FromLeStream, ToLeStream};
-use le_stream::{FromLeStream, ToLeStream};
-use num_traits::FromPrimitive;
+use bitflags::bitflags;
 
-pub use frame_format_version::FrameFormatVersion;
-
-use crate::frame::header::control::high_byte::frame_format_version::{
-    bit_swap, FRAME_FORMAT_VERSION_MASK_HIGH, FRAME_FORMAT_VERSION_MASK_LOW,
-};
-
-mod frame_format_version;
-
-const FRAME_FORMAT_VERSION_MASK: u8 =
-    FRAME_FORMAT_VERSION_MASK_LOW + FRAME_FORMAT_VERSION_MASK_HIGH;
-
-#[derive(Clone, Copy, Debug, Ord, PartialOrd, Eq, PartialEq, FromLeStream, ToLeStream)]
+#[derive(Clone, Copy, Debug, Default, Eq, PartialEq)]
 pub struct HighByte(u8);
 
-impl HighByte {
-    #[must_use]
-    pub fn frame_format_version(self) -> Option<FrameFormatVersion> {
-        FrameFormatVersion::from_u8(bit_swap(self.0 & FRAME_FORMAT_VERSION_MASK))
-    }
-
-    pub fn set_frame_format_version(&mut self, version: FrameFormatVersion) {
-        self.0 &= (0xFF ^ FRAME_FORMAT_VERSION_MASK) | bit_swap(version.into());
-    }
-}
-
-impl Default for HighByte {
-    fn default() -> Self {
-        Self(bit_swap(FrameFormatVersion::One.into()))
-    }
-}
-
-impl From<HighByte> for u8 {
-    fn from(low_byte: HighByte) -> Self {
-        low_byte.0
-    }
-}
-
-impl From<u8> for HighByte {
-    fn from(value: u8) -> Self {
-        Self(value)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::{FrameFormatVersion, HighByte};
-
-    #[test]
-    fn test_default() {
-        let high_byte = HighByte::default();
-        let byte: u8 = high_byte.into();
-        assert_eq!(byte, 0b0000_0001);
-    }
-
-    #[test]
-    fn test_set_frame_format_version() {
-        let mut high_byte = HighByte::default();
-
-        high_byte.set_frame_format_version(FrameFormatVersion::One);
-        let byte: u8 = high_byte.into();
-        assert_eq!(byte, 0b0000_0001);
-
-        high_byte.set_frame_format_version(FrameFormatVersion::Zero);
-        let byte: u8 = high_byte.into();
-        assert_eq!(byte, 0b0000_0000);
-    }
-
-    #[test]
-    fn get_frame_format_version() {
-        let high_byte = HighByte(0b0000_0000);
-        assert_eq!(
-            high_byte.frame_format_version(),
-            Some(FrameFormatVersion::Zero)
-        );
-
-        let high_byte = HighByte(0b0000_0001);
-        assert_eq!(
-            high_byte.frame_format_version(),
-            Some(FrameFormatVersion::One)
-        );
-
-        let high_byte = HighByte(0b0000_0010);
-        assert_eq!(high_byte.frame_format_version(), None);
-
-        let high_byte = HighByte(0b0000_0011);
-        assert_eq!(high_byte.frame_format_version(), None);
+bitflags! {
+    impl Command: u8 {
+        const SECURITY_ENABLED = 0b1000_0000;
+        const PADDING_ENABLED = 0b0100_0000;
+        const FRAME_FORMAT_VERSION_1 = 0b0000_0010;
+        const FRAME_FORMAT_VERSION_0 = 0b0000_0001;
     }
 }
--- src/frame/header/control/low_byte.rs
+++ src/frame/header/control/low_byte.rs
@@ -1,103 +1,49 @@
-pub use callback_type::CallbackType;
-use le_stream::derive::{FromLeStream, ToLeStream};
-use log::warn;
-use num_traits::FromPrimitive;
-pub use sleep_mode::SleepMode;
+use bitflags::bitflags;
 
-mod callback_type;
-mod sleep_mode;
+#[derive(Clone, Copy, Debug, Default, Eq, PartialEq)]
+pub struct Command(u8);
 
-const COMMAND_MASK: u8 = 0b1000_0000;
-const OVERFLOW_MASK: u8 = 0b0000_0001;
-const TRUNCATED_MASK: u8 = 0b0000_0010;
-const CALLBACK_PENDING_MASK: u8 = 0b0000_0100;
-const CALLBACK_TYPE_MASK_HIGH: u8 = 0b0001_0000;
-const CALLBACK_TYPE_MASK_LOW: u8 = 0b0000_1000;
-const CALLBACK_OFFSET_HIGH: u8 = 3;
-const CALLBACK_OFFSET_LOW: u8 = 2;
-const NETWORK_INDEX_MASK_HIGH: u8 = 0b0100_0000;
-const NETWORK_INDEX_MASK_LOW: u8 = 0b0010_0000;
-const NETWORK_INDEX_MASK: u8 = NETWORK_INDEX_MASK_HIGH + NETWORK_INDEX_MASK_LOW;
-const NETWORK_INDEX_OFFSET_HIGH: u8 = 6;
-const NETWORK_INDEX_OFFSET_LOW: u8 = 4;
-const SLEEP_MODE_MASK_LOW: u8 = 0b0000_0001;
-const SLEEP_MODE_MASK_HIGH: u8 = 0b0000_0010;
-const SLEEP_MODE_OFFSET: u8 = 1;
-
-#[derive(Clone, Copy, Debug, Default, Ord, PartialOrd, Eq, PartialEq, FromLeStream, ToLeStream)]
-pub struct LowByte(u8);
-
-impl LowByte {
-    #[must_use]
-    pub const fn is_command(self) -> bool {
-        self.0 & COMMAND_MASK == 0
-    }
-
-    #[must_use]
-    pub const fn is_response(self) -> bool {
-        !self.is_command()
-    }
-
-    #[must_use]
-    pub const fn network_index(self) -> u8 {
-        ((self.0 & NETWORK_INDEX_MASK_HIGH) >> NETWORK_INDEX_OFFSET_HIGH)
-            + ((self.0 & NETWORK_INDEX_MASK_LOW) >> NETWORK_INDEX_OFFSET_LOW)
-    }
-
-    pub fn set_network_index(&mut self, network_index: u8) {
-        let index = network_index & 0b11;
-
-        if index != network_index {
-            warn!("Network index truncated from {network_index:#04X} to {index:#04X}");
-        }
-
-        self.0 &= (0xFF ^ NETWORK_INDEX_MASK)
-            | (((index & (NETWORK_INDEX_MASK_HIGH >> NETWORK_INDEX_OFFSET_HIGH))
-                << NETWORK_INDEX_OFFSET_HIGH)
-                + ((index & (NETWORK_INDEX_MASK_LOW >> NETWORK_INDEX_OFFSET_LOW))
-                    << NETWORK_INDEX_OFFSET_LOW));
-    }
-
-    #[must_use]
-    pub fn callback_type(self) -> Option<CallbackType> {
-        CallbackType::from_u8(
-            ((self.0 & CALLBACK_TYPE_MASK_HIGH) >> CALLBACK_OFFSET_HIGH)
-                + ((self.0 & CALLBACK_TYPE_MASK_LOW) >> CALLBACK_OFFSET_LOW),
-        )
-    }
-
-    #[must_use]
-    pub const fn callback_pending(self) -> bool {
-        self.0 & CALLBACK_PENDING_MASK != 0
-    }
-
-    #[must_use]
-    pub const fn is_truncated(self) -> bool {
-        self.0 & TRUNCATED_MASK != 0
-    }
-
-    #[must_use]
-    pub const fn is_overflow(self) -> bool {
-        self.0 & OVERFLOW_MASK != 0
-    }
-
-    #[must_use]
-    pub fn sleep_mode(self) -> Option<SleepMode> {
-        SleepMode::from_u8(
-            ((self.0 & SLEEP_MODE_MASK_LOW) << SLEEP_MODE_OFFSET)
-                + ((self.0 & SLEEP_MODE_MASK_HIGH) >> SLEEP_MODE_OFFSET),
-        )
+bitflags! {
+    impl Command: u8 {
+        const IS_RESPONSE = 0b1000_0000;
+        const NETWORK_INDEX_1 = 0b0100_0000;
+        const NETWORK_INDEX_0 = 0b0010_0000;
+        const CALLBACK_TYPE_1 = 0b0001_0000;
+        const CALLBACK_TYPE_0 = 0b0000_1000;
+        const CALLBACK_PENDING = 0b0000_0100;
+        const TRUNCATED = 0b0000_0010;
+        const OVERFLOW = 0b0000_0001;
     }
 }
 
-impl From<LowByte> for u8 {
-    fn from(low_byte: LowByte) -> Self {
-        low_byte.0
+#[derive(Clone, Copy, Debug, Default, Eq, PartialEq)]
+pub struct Response(u8);
+
+bitflags! {
+    impl Response: u8 {
+        const IS_RESPONSE = 0b1000_0000;
+        const NETWORK_INDEX_1 = 0b0100_0000;
+        const NETWORK_INDEX_0 = 0b0010_0000;
+        const CALLBACK_TYPE_1 = 0b0001_0000;
+        const CALLBACK_TYPE_0 = 0b0000_1000;
+        const CALLBACK_PENDING = 0b0000_0100;
+        const TRUNCATED = 0b0000_0010;
+        const OVERFLOW = 0b0000_0001;
     }
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub enum LowByte {
+    Command(Command),
+    Response(Response),
 }
 
 impl From<u8> for LowByte {
-    fn from(value: u8) -> Self {
-        Self(value)
+    fn from(byte: u8) -> Self {
+        if byte & Response::IS_RESPONSE.bits() != 0 {
+            LowByte::Response(Response::from_bits_retain(byte))
+        } else {
+            LowByte::Command(Command::from_bits_retain(byte))
+        }
     }
 }

cd5cf60c3804fc209cd8dd1e0840f40788b41e2b
Richard Neumann <neumann@paulmann.de>
2024-10-12 19:55:24 +0200

    Implement get_parent_classification_enabled()

--- src/frame/parameters/wwah/get_parent_classification_enabled.rs
+++ src/frame/parameters/wwah/get_parent_classification_enabled.rs
@@ -1,30 +1,28 @@
+use crate::frame::Parameter;
 use le_stream::derive::{FromLeStream, ToLeStream};
 
 const ID: u16 = 0x00F0;
 
-#[derive(Clone, Debug, Eq, PartialEq, FromLeStream, ToLeStream)]
+#[derive(Clone, Debug, Eq, PartialEq, ToLeStream)]
 pub struct Command;
 
-impl Command {
-    #[must_use]
-    pub const fn new() -> Self {
-        Self {}
-    }
+impl Parameter for Command {
+    type Id = u16;
+    const ID: Self::Id = ID;
 }
 
-#[derive(Clone, Debug, Eq, PartialEq, FromLeStream, ToLeStream)]
+#[derive(Clone, Debug, Eq, PartialEq, FromLeStream)]
 pub struct Response {
     enabled: bool,
 }
 
-impl Response {
-    #[must_use]
-    pub const fn new(enabled: bool) -> Self {
-        Self { enabled }
-    }
+impl Parameter for Response {
+    type Id = u16;
+    const ID: Self::Id = ID;
+}
 
-    #[must_use]
-    pub const fn enabled(&self) -> bool {
-        self.enabled
+impl From<Response> for bool {
+    fn from(response: Response) -> Self {
+        response.enabled
     }
 }
--- src/transport/ezsp/wwah.rs
+++ src/transport/ezsp/wwah.rs
@@ -1,9 +1,17 @@
-use crate::frame::parameters::wwah::set_parent_classification_enabled;
+use crate::frame::parameters::wwah::{
+    get_parent_classification_enabled, set_parent_classification_enabled,
+};
 use crate::{Error, Transport};
 use std::future::Future;
 
 /// The `Wwah` trait provides an interface for the Work With All Hubs (WWAH) protocol.
 pub trait Wwah {
+    /// Gets whether to use parent classification when processing beacons during a join or rejoin.
+    ///
+    /// Parent classification considers whether a received beacon indicates trust center
+    /// connectivity and long uptime on the network.
+    fn get_parent_classification_enabled(&mut self) -> impl Future<Output = Result<bool, Error>>;
+
     /// Sets whether to use parent classification when processing beacons during a join or rejoin.
     ///
     /// Parent classification considers whether a received beacon indicates trust center
@@ -24,5 +32,13 @@ where
         )
         .await
         .map(drop)
+    }
+
+    async fn get_parent_classification_enabled(&mut self) -> Result<bool, Error> {
+        self.communicate::<_, get_parent_classification_enabled::Response>(
+            get_parent_classification_enabled::Command,
+        )
+        .await
+        .map(Into::into)
     }
 }

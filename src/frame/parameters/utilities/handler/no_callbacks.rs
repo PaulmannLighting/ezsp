2bd41356daf337965c72d58d5deca7faec96712f
Richard Neumann <neumann@paulmann.de>
2024-10-12 13:53:01 +0200

    Implement Parameter for no_callbacks:Response

--- src/frame/parameters/utilities/handler/no_callbacks.rs
+++ src/frame/parameters/utilities/handler/no_callbacks.rs
@@ -1,3 +1,4 @@
+use crate::frame::Parameter;
 use le_stream::derive::FromLeStream;
 
 const ID: u16 = 0x0007;
@@ -5,3 +6,8 @@ const ID: u16 = 0x0007;
 /// The response to this command can be any of the callback responses.
 #[derive(Clone, Debug, Eq, PartialEq, FromLeStream)]
 pub struct Response;
+
+impl Parameter for Response {
+    type Id = u16;
+    const ID: Self::Id = ID;
+}

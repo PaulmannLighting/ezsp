3077b0a3b332df33137c3b26ac8ea360bde13bc0
Richard Neumann <neumann@paulmann.de>
2024-10-12 13:52:18 +0200

    Add docstring

--- src/frame/parameters/utilities/handler/no_callbacks.rs
+++ src/frame/parameters/utilities/handler/no_callbacks.rs
@@ -2,5 +2,6 @@ use le_stream::derive::FromLeStream;
 
 const ID: u16 = 0x0007;
 
+/// The response to this command can be any of the callback responses.
 #[derive(Clone, Debug, Eq, PartialEq, FromLeStream)]
 pub struct Response;

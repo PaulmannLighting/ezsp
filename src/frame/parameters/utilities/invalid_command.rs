2fe715678a27bb13b06c78ac343dcaf670ee05d3
Richard Neumann <neumann@paulmann.de>
2024-10-12 11:36:01 +0200

    Add docs to invalid_command::Response

--- src/frame/parameters/utilities/invalid_command.rs
+++ src/frame/parameters/utilities/invalid_command.rs
@@ -4,12 +4,18 @@ use le_stream::derive::{FromLeStream, ToLeStream};
 
 const ID: u16 = 0x0058;
 
+/// Response sent by the NCP when it received an invalid command.
 #[derive(Clone, Debug, Eq, PartialEq, FromLeStream, ToLeStream)]
 pub struct Response {
     reason: u8,
 }
 
 impl Response {
+    /// Returns the reason for the invalid command response.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the reason is not a valid [`Status`].
     pub fn reason(&self) -> Result<Status, u8> {
         Status::try_from(self.reason)
     }

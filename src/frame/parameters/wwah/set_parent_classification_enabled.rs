23baa51b4e9e8573675c260d5b45c094289c1794
Richard Neumann <neumann@paulmann.de>
2024-10-12 19:52:49 +0200

    Implement set_parent_classification_enabled()

--- src/frame/parameters/wwah/set_parent_classification_enabled.rs
+++ src/frame/parameters/wwah/set_parent_classification_enabled.rs
@@ -1,8 +1,9 @@
+use crate::frame::Parameter;
 use le_stream::derive::{FromLeStream, ToLeStream};
 
 const ID: u16 = 0x00E7;
 
-#[derive(Clone, Debug, Eq, PartialEq, FromLeStream, ToLeStream)]
+#[derive(Clone, Debug, Eq, PartialEq, ToLeStream)]
 pub struct Command {
     enabled: bool,
 }
@@ -12,19 +13,17 @@ impl Command {
     pub const fn new(enabled: bool) -> Self {
         Self { enabled }
     }
-
-    #[must_use]
-    pub const fn enabled(&self) -> bool {
-        self.enabled
-    }
 }
 
-#[derive(Clone, Debug, Eq, PartialEq, FromLeStream, ToLeStream)]
+impl Parameter for Command {
+    type Id = u16;
+    const ID: Self::Id = ID;
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, FromLeStream)]
 pub struct Response;
 
-impl Response {
-    #[must_use]
-    pub const fn new() -> Self {
-        Self {}
-    }
+impl Parameter for Response {
+    type Id = u16;
+    const ID: Self::Id = ID;
 }
--- src/transport/ezsp/wwah.rs
+++ src/transport/ezsp/wwah.rs
@@ -1,6 +1,28 @@
-use crate::Transport;
+use crate::frame::parameters::wwah::set_parent_classification_enabled;
+use crate::{Error, Transport};
+use std::future::Future;
 
 /// The `Wwah` trait provides an interface for the Work With All Hubs (WWAH) protocol.
-pub trait Wwah {}
+pub trait Wwah {
+    /// Sets whether to use parent classification when processing beacons during a join or rejoin.
+    ///
+    /// Parent classification considers whether a received beacon indicates trust center
+    /// connectivity and long uptime on the network.
+    fn set_parent_classification_enabled(
+        &mut self,
+        enabled: bool,
+    ) -> impl Future<Output = Result<(), Error>>;
+}
 
-impl<T> Wwah for T where T: Transport {}
+impl<T> Wwah for T
+where
+    T: Transport,
+{
+    async fn set_parent_classification_enabled(&mut self, enabled: bool) -> Result<(), Error> {
+        self.communicate::<_, set_parent_classification_enabled::Response>(
+            set_parent_classification_enabled::Command::new(enabled),
+        )
+        .await
+        .map(drop)
+    }
+}

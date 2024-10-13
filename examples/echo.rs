2e76cc463cd8edb88e36e32e4c337f1182613973
Richard Neumann <neumann@paulmann.de>
2024-10-13 14:24:24 +0200

    Add option to keep listening

--- examples/echo.rs
+++ examples/echo.rs
@@ -19,6 +19,8 @@ struct Args {
     texts: Vec<String>,
     #[arg(short, long, default_value_t = DEFAULT_VERSION, help = "EZSP version to negotiate")]
     version: u8,
+    #[arg(short, long, help = "Keep listening for incoming messages")]
+    keep_listening: bool,
 }
 
 #[tokio::main]
@@ -26,19 +28,19 @@ async fn main() {
     env_logger::init();
     let args = Args::parse();
 
-    match open(args.tty, BaudRate::RstCts, FlowControl::Software) {
-        Ok(serial_port) => run(serial_port, &args.texts, args.version).await,
+    match open(args.tty.clone(), BaudRate::RstCts, FlowControl::Software) {
+        Ok(serial_port) => run(serial_port, args).await,
         Err(error) => error!("{error}"),
     }
 }
 
-async fn run(serial_port: impl SerialPort + Sized + 'static, texts: &[String], version: u8) {
+async fn run(serial_port: impl SerialPort + Sized + 'static, args: Args) {
     let (ash, transceiver) = make_pair::<8, _>(serial_port, None);
     let running = Arc::new(AtomicBool::new(true));
-    let _transceiver_thread = spawn(|| transceiver.run(running));
+    let transceiver_thread = spawn(|| transceiver.run(running));
     let mut ezsp = Ashv2::new(ash);
 
-    match ezsp.negotiate_version(version).await {
+    match ezsp.negotiate_version(args.version).await {
         Ok(version) => {
             info!(
                 "Negotiated protocol version: {:#04X}",
@@ -52,7 +54,7 @@ async fn run(serial_port: impl SerialPort + Sized + 'static, texts: &[String], v
         }
     }
 
-    for text in texts {
+    for text in args.texts {
         match ezsp.echo(text.bytes().collect()).await {
             Ok(echo) => match String::from_utf8(echo.to_vec()) {
                 Ok(echo) => {
@@ -66,5 +68,11 @@ async fn run(serial_port: impl SerialPort + Sized + 'static, texts: &[String], v
                 error!("{error}");
             }
         }
+    }
+
+    if args.keep_listening {
+        transceiver_thread
+            .join()
+            .expect("Transceiver thread panicked");
     }
 }

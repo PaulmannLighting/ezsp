f5cb2abd4d2382911d2cdfd486631dd3d3120490
Richard Neumann <neumann@paulmann.de>
2024-10-14 12:43:12 +0200

    Refactor imports

--- src/resolve.rs
+++ src/resolve.rs
@@ -1,5 +1,5 @@
 use crate::error::Invalid;
-use crate::{Error, Result};
+use crate::Error;
 
 /// Resolve types into a result of a certain output type.
 pub trait Resolve {
@@ -12,13 +12,13 @@ pub trait Resolve {
     ///
     /// # Errors
     /// Return [`Error`] in case of errors.
-    fn resolve(self) -> Result<Self::Output>;
+    fn resolve(self) -> Result<Self::Output, Error>;
 }
 
-impl Resolve for std::result::Result<siliconlabs::Status, u32> {
+impl Resolve for Result<siliconlabs::Status, u32> {
     type Output = ();
 
-    fn resolve(self) -> Result<Self::Output> {
+    fn resolve(self) -> Result<Self::Output, Error> {
         match self {
             Ok(status) => {
                 if status == siliconlabs::Status::Ok {

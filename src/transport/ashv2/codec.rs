570c5f8267c12b8850f0faafee41a27c6e0dcc7d
Richard Neumann <neumann@paulmann.de>
2024-10-15 11:53:56 +0200

    Fix codec

--- src/transport/ashv2/codec.rs
+++ src/transport/ashv2/codec.rs
@@ -106,10 +106,16 @@ where
     fn encode(&mut self, item: Frame<C, P>, dst: &mut BytesMut) -> Result<(), Self::Error> {
         let header: Vec<u8> = item.header().to_le_stream().collect();
         let parameters: Vec<u8> = item.parameters().to_le_stream().collect();
+        let chunk_size = MAX_PAYLOAD_SIZE.saturating_sub(header.len());
 
-        for chunk in parameters.chunks(MAX_PAYLOAD_SIZE.saturating_sub(header.len())) {
+        if parameters.len() > chunk_size {
+            for chunk in parameters.chunks(chunk_size) {
+                dst.extend_from_slice(&header);
+                dst.extend_from_slice(chunk);
+            }
+        } else {
             dst.extend_from_slice(&header);
-            dst.extend_from_slice(chunk);
+            dst.extend_from_slice(&parameters);
         }
 
         Ok(())

dcdc2a9d56cf48ba6657b212871243d5179f6017
Richard Neumann <neumann@paulmann.de>
2024-10-16 02:08:39 +0200

    Fix add_endpoint::Command serialization

--- src/frame/parameters/configuration/add_endpoint.rs
+++ src/frame/parameters/configuration/add_endpoint.rs
@@ -1,5 +1,5 @@
 use crate::Resolve;
-use le_stream::derive::FromLeStream;
+use le_stream::derive::{FromLeStream, ToLeStream};
 use le_stream::ToLeStream;
 use std::array::IntoIter;
 use std::iter::{Chain, FlatMap};
@@ -9,16 +9,14 @@ use crate::frame::Parameter;
 use crate::types::ByteSizedVec;
 
 const ID: u16 = 0x0002;
-const SIZE: usize = 1 + 2 + 2 + 1 + 2 * (2 * u8::MAX as usize);
 
-#[derive(Clone, Debug, Eq, PartialEq)]
+#[derive(Clone, Debug, Eq, PartialEq, ToLeStream)]
 pub struct Command {
     endpoint: u8,
     profile_id: u16,
     device_id: u16,
     app_flags: u8,
-    input_clusters: ByteSizedVec<u16>,
-    output_clusters: ByteSizedVec<u16>,
+    clusters: Clusters,
 }
 
 impl Command {
@@ -36,48 +34,42 @@ impl Command {
             profile_id,
             device_id,
             app_flags,
-            input_clusters: input_cluster_list,
-            output_clusters: output_cluster_list,
+            clusters: Clusters {
+                input_clusters: input_cluster_list,
+                output_clusters: output_cluster_list,
+            },
         }
     }
 }
 
-impl ToLeStream for Command {
+#[derive(Clone, Debug, Eq, PartialEq)]
+struct Clusters {
+    input_clusters: ByteSizedVec<u16>,
+    output_clusters: ByteSizedVec<u16>,
+}
+
+impl ToLeStream for Clusters {
     type Iter = Chain<
         Chain<
-            Chain<
-                Chain<
-                    Chain<
-                        Chain<Chain<IntoIter<u8, 1>, IntoIter<u8, 2>>, IntoIter<u8, 2>>,
-                        IntoIter<u8, 1>,
-                    >,
-                    IntoIter<u8, 1>,
-                >,
-                FlatMap<<ByteSizedVec<u16> as IntoIterator>::IntoIter, [u8; 2], fn(u16) -> [u8; 2]>,
-            >,
-            IntoIter<u8, 1>,
+            Chain<IntoIter<u8, 1>, IntoIter<u8, 1>>,
+            FlatMap<<ByteSizedVec<u16> as IntoIterator>::IntoIter, [u8; 2], fn(u16) -> [u8; 2]>,
         >,
         FlatMap<<ByteSizedVec<u16> as IntoIterator>::IntoIter, [u8; 2], fn(u16) -> [u8; 2]>,
     >;
 
     fn to_le_stream(self) -> Self::Iter {
-        self.endpoint
+        (self.input_clusters.len() as u8)
             .to_le_stream()
-            .into_iter()
-            .chain(self.profile_id.to_le_stream())
-            .chain(self.device_id.to_le_stream())
-            .chain(self.app_flags.to_le_stream())
-            .chain((self.input_clusters.len() as u8).to_le_stream())
+            .chain((self.output_clusters.len() as u8).to_le_stream())
             .chain(
                 self.input_clusters
                     .into_iter()
-                    .flat_map(u16::to_le_bytes as fn(u16) -> [u8; 2]),
+                    .flat_map(u16::to_le_bytes as _),
             )
-            .chain((self.output_clusters.len() as u8).to_le_stream())
             .chain(
                 self.output_clusters
                     .into_iter()
-                    .flat_map(u16::to_le_bytes as fn(u16) -> [u8; 2]),
+                    .flat_map(u16::to_le_bytes as _),
             )
     }
 }

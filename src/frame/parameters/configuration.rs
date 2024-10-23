54669f586bef98f02cc60288ea91223b043f873c
Richard Neumann <neumann@paulmann.de>
2024-10-23 21:38:58 +0200

    Implement Parsable for configuration

--- src/frame/parameters/configuration.rs
+++ src/frame/parameters/configuration.rs
@@ -1,5 +1,11 @@
 //! Configuration Frames
 
+use le_stream::FromLeStream;
+
+use crate::error::Decode;
+use crate::frame::parsable::Parsable;
+use crate::frame::Parameter;
+
 pub(crate) mod add_endpoint;
 pub(crate) mod get_configuration_value;
 pub(crate) mod get_extended_value;
@@ -46,4 +52,62 @@ pub enum Response {
     SetValue(set_value::Response),
     Version(version::Response),
     WriteAttribute(write_attribute::Response),
+}
+
+impl Parsable for Response {
+    fn parse_from_le_stream<T>(id: u16, stream: T) -> Result<Self, Decode>
+    where
+        T: Iterator<Item = u8>,
+    {
+        const VERSION_ID: u16 = <version::Response as Parameter>::ID as u16;
+
+        match id {
+            <add_endpoint::Response as Parameter>::ID => Ok(Response::AddEndpoint(
+                add_endpoint::Response::from_le_stream_exact(stream)?,
+            )),
+            <get_configuration_value::Response as Parameter>::ID => {
+                Ok(Response::GetConfigurationValue(
+                    get_configuration_value::Response::from_le_stream_exact(stream)?,
+                ))
+            }
+            <get_extended_value::Response as Parameter>::ID => Ok(Response::GetExtendedValue(
+                get_extended_value::Response::from_le_stream_exact(stream)?,
+            )),
+            <get_policy::Response as Parameter>::ID => Ok(Response::GetPolicy(
+                get_policy::Response::from_le_stream_exact(stream)?,
+            )),
+            <get_value::Response as Parameter>::ID => Ok(Response::GetValue(
+                get_value::Response::from_le_stream_exact(stream)?,
+            )),
+            <read_attribute::Response as Parameter>::ID => Ok(Response::ReadAttribute(
+                read_attribute::Response::from_le_stream_exact(stream)?,
+            )),
+            <send_pan_id_update::Response as Parameter>::ID => Ok(Response::SendPanIdUpdate(
+                send_pan_id_update::Response::from_le_stream_exact(stream)?,
+            )),
+            <set_configuration_value::Response as Parameter>::ID => {
+                Ok(Response::SetConfigurationValue(
+                    set_configuration_value::Response::from_le_stream_exact(stream)?,
+                ))
+            }
+            <set_passive_ack_config::Response as Parameter>::ID => {
+                Ok(Response::SetPassiveAckConfig(
+                    set_passive_ack_config::Response::from_le_stream_exact(stream)?,
+                ))
+            }
+            <set_policy::Response as Parameter>::ID => Ok(Response::SetPolicy(
+                set_policy::Response::from_le_stream_exact(stream)?,
+            )),
+            <set_value::Response as Parameter>::ID => Ok(Response::SetValue(
+                set_value::Response::from_le_stream_exact(stream)?,
+            )),
+            VERSION_ID => Ok(Response::Version(version::Response::from_le_stream_exact(
+                stream,
+            )?)),
+            <write_attribute::Response as Parameter>::ID => Ok(Response::WriteAttribute(
+                write_attribute::Response::from_le_stream_exact(stream)?,
+            )),
+            _ => Err(Decode::InvalidFrameId(id)),
+        }
+    }
 }

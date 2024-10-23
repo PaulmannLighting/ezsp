f420d07df028ff4b3191ce513054ac8d2548dfa7
Richard Neumann <neumann@paulmann.de>
2024-10-23 22:36:37 +0200

    Make all submodules public

--- src/frame/parameters/mfglib.rs
+++ src/frame/parameters/mfglib.rs
@@ -1,17 +1,17 @@
 //! Mfglib Frames
 
-pub(crate) mod end;
-pub(crate) mod get_channel;
-pub(crate) mod get_power;
+pub mod end;
+pub mod get_channel;
+pub mod get_power;
 pub mod handler;
-pub(crate) mod send_packet;
-pub(crate) mod set_channel;
-pub(crate) mod set_power;
-pub(crate) mod start;
-pub(crate) mod start_stream;
-pub(crate) mod start_tone;
-pub(crate) mod stop_stream;
-pub(crate) mod stop_tone;
+pub mod send_packet;
+pub mod set_channel;
+pub mod set_power;
+pub mod start;
+pub mod start_stream;
+pub mod start_tone;
+pub mod stop_stream;
+pub mod stop_tone;
 
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Response {

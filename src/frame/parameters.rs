3728235d2b8eda9074ed13d17658f816ca34f88a
Richard Neumann <neumann@paulmann.de>
2024-10-23 22:04:28 +0200

    Remove command

--- src/frame/parameters.rs
+++ src/frame/parameters.rs
@@ -15,40 +15,6 @@ pub mod utilities;
 pub mod wwah;
 pub mod zll;
 
-/// `EZSP` command parameters.
-#[derive(Clone, Debug, Eq, PartialEq)]
-#[allow(clippy::large_enum_variant)]
-pub enum Command {
-    /// Command parameters for binding frames.
-    Binding(binding::Command),
-    /// Command parameters for bootloader frames.
-    Bootloader(bootloader::Command),
-    /// Command parameters for certificate-based key exchange (CBKE) frames.
-    Cbke(cbke::Command),
-    /// Command parameters for configuration frames.
-    Configuration(configuration::Command),
-    /// Command parameters for green power frames.
-    GreenPower(green_power::Command),
-    /// Command parameters for messaging frames.
-    Messaging(messaging::Command),
-    /// Command parameters for Manufacturing Test Library (`MfgLib`) frames.
-    MfgLib(mfglib::Command),
-    /// Command parameters for networking frames.
-    Networking(networking::Command),
-    /// Command parameters for security frames.
-    Security(security::Command),
-    /// Command parameters for token interface frames.
-    TokenInterface(token_interface::Command),
-    /// Command parameters for trust center frames.
-    TrustCenter(trust_center::Command),
-    /// Command parameters for utilities frames.
-    Utilities(utilities::Command),
-    /// Command parameters for Wireless Workgroup for Home Automation (WWAH) frames.
-    Wwah(wwah::Command),
-    /// Command parameters for Zigbee Light Link (ZLL) frames.
-    Zll(zll::Command),
-}
-
 /// `EZSP` response parameters.
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Response {

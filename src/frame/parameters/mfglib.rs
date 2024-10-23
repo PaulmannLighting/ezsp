0c3710110d9bcf3f1e7d2aaa902156c442cc2fc8
Richard Neumann <neumann@paulmann.de>
2024-10-23 22:39:03 +0200

    Implement Parsable for mfglib

--- src/frame/parameters/mfglib.rs
+++ src/frame/parameters/mfglib.rs
@@ -1,5 +1,11 @@
 //! Mfglib Frames
 
+use le_stream::FromLeStream;
+
+use crate::error::Decode;
+use crate::frame::parsable::Parsable;
+use crate::frame::Parameter;
+
 pub mod end;
 pub mod get_channel;
 pub mod get_power;
@@ -27,4 +33,51 @@ pub enum Response {
     StopStream(stop_stream::Response),
     StopTone(stop_tone::Response),
     Handler(handler::Handler),
+}
+
+impl Parsable for Response {
+    fn parse_from_le_stream<T>(id: u16, stream: T) -> Result<Self, Decode>
+    where
+        T: Iterator<Item = u8>,
+    {
+        match id {
+            <end::Response as Parameter>::ID => {
+                Ok(Self::End(end::Response::from_le_stream_exact(stream)?))
+            }
+            <get_channel::Response as Parameter>::ID => Ok(Self::GetChannel(
+                get_channel::Response::from_le_stream_exact(stream)?,
+            )),
+            <get_power::Response as Parameter>::ID => Ok(Self::GetPower(
+                get_power::Response::from_le_stream_exact(stream)?,
+            )),
+            <send_packet::Response as Parameter>::ID => Ok(Self::SendPacket(
+                send_packet::Response::from_le_stream_exact(stream)?,
+            )),
+            <set_channel::Response as Parameter>::ID => Ok(Self::SetChannel(
+                set_channel::Response::from_le_stream_exact(stream)?,
+            )),
+            <set_power::Response as Parameter>::ID => Ok(Self::SetPower(
+                set_power::Response::from_le_stream_exact(stream)?,
+            )),
+            <start::Response as Parameter>::ID => {
+                Ok(Self::Start(start::Response::from_le_stream_exact(stream)?))
+            }
+            <start_stream::Response as Parameter>::ID => Ok(Self::StartStream(
+                start_stream::Response::from_le_stream_exact(stream)?,
+            )),
+            <start_tone::Response as Parameter>::ID => Ok(Self::StartTone(
+                start_tone::Response::from_le_stream_exact(stream)?,
+            )),
+            <stop_stream::Response as Parameter>::ID => Ok(Self::StopStream(
+                stop_stream::Response::from_le_stream_exact(stream)?,
+            )),
+            <stop_tone::Response as Parameter>::ID => Ok(Self::StopTone(
+                stop_tone::Response::from_le_stream_exact(stream)?,
+            )),
+            <handler::Rx as Parameter>::ID => Ok(Self::Handler(handler::Handler::Rx(
+                handler::Rx::from_le_stream_exact(stream)?,
+            ))),
+            _ => Err(Decode::InvalidFrameId(id)),
+        }
+    }
 }

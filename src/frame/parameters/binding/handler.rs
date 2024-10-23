5aaeba9a66811962a7607a0776df0e0356cb0e0f
Richard Neumann <neumann@paulmann.de>
2024-10-23 16:44:48 +0200

    Implement Decode for binding::Handler

--- src/frame/parameters/binding/handler.rs
+++ src/frame/parameters/binding/handler.rs
@@ -1,10 +1,16 @@
 //! Handlers for the binding commands.
 
-mod remote_delete_binding;
-mod remote_set_binding;
+use le_stream::FromLeStream;
+
+use crate::error::Decode;
+use crate::frame::parse::Parse;
+use crate::frame::Parameter;
 
 pub use remote_delete_binding::Handler as RemoteDeleteBinding;
 pub use remote_set_binding::Handler as RemoteSetBinding;
+
+mod remote_delete_binding;
+mod remote_set_binding;
 
 /// The handler for the binding commands.
 #[allow(variant_size_differences, clippy::large_enum_variant)]
@@ -25,5 +31,22 @@ impl From<RemoteDeleteBinding> for Handler {
 impl From<RemoteSetBinding> for Handler {
     fn from(handler: RemoteSetBinding) -> Self {
         Self::RemoteSetBinding(handler)
+    }
+}
+
+impl Parse for Handler {
+    fn parse<T>(id: u16, stream: T) -> Result<Self, Decode>
+    where
+        T: Iterator<Item = u8>,
+    {
+        match Some(id) {
+            <RemoteDeleteBinding as Parameter>::ID => Ok(Self::RemoteDeleteBinding(
+                RemoteDeleteBinding::from_le_stream_exact(stream)?,
+            )),
+            <RemoteSetBinding as Parameter>::ID => Ok(Self::RemoteSetBinding(
+                RemoteSetBinding::from_le_stream_exact(stream)?,
+            )),
+            _ => Err(Decode::InvalidFrameId(id)),
+        }
     }
 }

3bde55a14ccda03aed64cd3e10e0d94ac2f1ec3e
Richard Neumann <neumann@paulmann.de>
2024-10-24 09:09:15 +0200

    Implement parsable for networking

--- src/frame/parameters/networking.rs
+++ src/frame/parameters/networking.rs
@@ -1,5 +1,11 @@
 //! Networking Frames
 
+use le_stream::FromLeStream;
+
+use crate::error::Decode;
+use crate::frame::parsable::Parsable;
+use crate::frame::Parameter;
+
 pub mod child_id;
 pub mod clear_stored_beacons;
 pub mod energy_scan_request;
@@ -109,4 +115,222 @@ pub enum Response {
     StartScan(start_scan::Response),
     StopScan(stop_scan::Response),
     Handler(handler::Handler),
+}
+
+impl Parsable for Response {
+    fn parse_from_le_stream<T>(id: u16, stream: T) -> Result<Self, Decode>
+    where
+        T: Iterator<Item = u8>,
+    {
+        match id {
+            <child_id::Response as Parameter>::ID => Ok(Self::ChildId(
+                child_id::Response::from_le_stream_exact(stream)?,
+            )),
+            <clear_stored_beacons::Response as Parameter>::ID => Ok(Self::ClearStoredBeacons(
+                clear_stored_beacons::Response::from_le_stream_exact(stream)?,
+            )),
+            <energy_scan_request::Response as Parameter>::ID => Ok(Self::EnergyScanRequest(
+                energy_scan_request::Response::from_le_stream_exact(stream)?,
+            )),
+            <find_and_rejoin_network::Response as Parameter>::ID => Ok(Self::FindAndRejoinNetwork(
+                find_and_rejoin_network::Response::from_le_stream_exact(stream)?,
+            )),
+            <find_unused_pan_id::Response as Parameter>::ID => Ok(Self::FindUnusedPanId(
+                find_unused_pan_id::Response::from_le_stream_exact(stream)?,
+            )),
+            <form_network::Response as Parameter>::ID => Ok(Self::FormNetwork(
+                form_network::Response::from_le_stream_exact(stream)?,
+            )),
+            <get_child_data::Response as Parameter>::ID => Ok(Self::GetChildData(
+                get_child_data::Response::from_le_stream_exact(stream)?,
+            )),
+            <get_current_duty_cycle::Response as Parameter>::ID => Ok(Self::GetCurrentDutyCycle(
+                get_current_duty_cycle::Response::from_le_stream_exact(stream)?,
+            )),
+            <get_duty_cycle_limits::Response as Parameter>::ID => Ok(Self::GetDutyCycleLimits(
+                get_duty_cycle_limits::Response::from_le_stream_exact(stream)?,
+            )),
+            <get_duty_cycle_state::Response as Parameter>::ID => Ok(Self::GetDutyCycleState(
+                get_duty_cycle_state::Response::from_le_stream_exact(stream)?,
+            )),
+            <get_first_beacon::Response as Parameter>::ID => Ok(Self::GetFirstBeacon(
+                get_first_beacon::Response::from_le_stream_exact(stream)?,
+            )),
+            <get_logical_channel::Response as Parameter>::ID => Ok(Self::GetLogicalChannel(
+                get_logical_channel::Response::from_le_stream_exact(stream)?,
+            )),
+            <get_neighbor::Response as Parameter>::ID => Ok(Self::GetNeighbor(
+                get_neighbor::Response::from_le_stream_exact(stream)?,
+            )),
+            <get_neighbor_frame_counter::Response as Parameter>::ID => {
+                Ok(Self::GetNeighborFrameCounter(
+                    get_neighbor_frame_counter::Response::from_le_stream_exact(stream)?,
+                ))
+            }
+            <get_network_parameters::Response as Parameter>::ID => Ok(Self::GetNetworkParameters(
+                get_network_parameters::Response::from_le_stream_exact(stream)?,
+            )),
+            <get_next_beacon::Response as Parameter>::ID => Ok(Self::GetNextBeacon(
+                get_next_beacon::Response::from_le_stream_exact(stream)?,
+            )),
+            <get_num_stored_beacons::Response as Parameter>::ID => Ok(Self::GetNumStoredBeacons(
+                get_num_stored_beacons::Response::from_le_stream_exact(stream)?,
+            )),
+            <get_parent_child_parameters::Response as Parameter>::ID => {
+                Ok(Self::GetParentChildParameters(
+                    get_parent_child_parameters::Response::from_le_stream_exact(stream)?,
+                ))
+            }
+            <get_radio_channel::Response as Parameter>::ID => Ok(Self::GetRadioChannel(
+                get_radio_channel::Response::from_le_stream_exact(stream)?,
+            )),
+            <get_radio_parameters::Response as Parameter>::ID => Ok(Self::GetRadioParameters(
+                get_radio_parameters::Response::from_le_stream_exact(stream)?,
+            )),
+            <get_route_table_entry::Response as Parameter>::ID => Ok(Self::GetRouteTableEntry(
+                get_route_table_entry::Response::from_le_stream_exact(stream)?,
+            )),
+            <get_routing_shortcut_threshold::Response as Parameter>::ID => {
+                Ok(Self::GetRoutingShortcutThreshold(
+                    get_routing_shortcut_threshold::Response::from_le_stream_exact(stream)?,
+                ))
+            }
+            <get_source_route_table_entry::Response as Parameter>::ID => {
+                Ok(Self::GetSourceRouteTableEntry(
+                    get_source_route_table_entry::Response::from_le_stream_exact(stream)?,
+                ))
+            }
+            <get_source_route_table_filled_size::Response as Parameter>::ID => {
+                Ok(Self::GetSourceRouteTableFilledSize(
+                    get_source_route_table_filled_size::Response::from_le_stream_exact(stream)?,
+                ))
+            }
+            <get_source_route_table_total_size::Response as Parameter>::ID => {
+                Ok(Self::GetSourceRouteTableTotalSize(
+                    get_source_route_table_total_size::Response::from_le_stream_exact(stream)?,
+                ))
+            }
+            <id::Response as Parameter>::ID => {
+                Ok(Self::Id(id::Response::from_le_stream_exact(stream)?))
+            }
+            <join_network::Response as Parameter>::ID => Ok(Self::JoinNetwork(
+                join_network::Response::from_le_stream_exact(stream)?,
+            )),
+            <join_network_directly::Response as Parameter>::ID => Ok(Self::JoinNetworkDirectly(
+                join_network_directly::Response::from_le_stream_exact(stream)?,
+            )),
+            <leave_network::Response as Parameter>::ID => Ok(Self::LeaveNetwork(
+                leave_network::Response::from_le_stream_exact(stream)?,
+            )),
+            <multi_phy_set_radio_channel::Response as Parameter>::ID => {
+                Ok(Self::MultiPhySetRadioChannel(
+                    multi_phy_set_radio_channel::Response::from_le_stream_exact(stream)?,
+                ))
+            }
+            <multi_phy_set_radio_power::Response as Parameter>::ID => {
+                Ok(Self::MultiPhySetRadioPower(
+                    multi_phy_set_radio_power::Response::from_le_stream_exact(stream)?,
+                ))
+            }
+            <multi_phy_start::Response as Parameter>::ID => Ok(Self::MultiPhyStart(
+                multi_phy_start::Response::from_le_stream_exact(stream)?,
+            )),
+            <multi_phy_stop::Response as Parameter>::ID => Ok(Self::MultiPhyStop(
+                multi_phy_stop::Response::from_le_stream_exact(stream)?,
+            )),
+            <neighbor_count::Response as Parameter>::ID => Ok(Self::NeighborCount(
+                neighbor_count::Response::from_le_stream_exact(stream)?,
+            )),
+            <network_init::Response as Parameter>::ID => Ok(Self::NetworkInit(
+                network_init::Response::from_le_stream_exact(stream)?,
+            )),
+            <network_state::Response as Parameter>::ID => Ok(Self::NetworkState(
+                network_state::Response::from_le_stream_exact(stream)?,
+            )),
+            <permit_joining::Response as Parameter>::ID => Ok(Self::PermitJoining(
+                permit_joining::Response::from_le_stream_exact(stream)?,
+            )),
+            <send_link_power_delta_request::Response as Parameter>::ID => {
+                Ok(Self::SendLinkPowerDeltaRequest(
+                    send_link_power_delta_request::Response::from_le_stream_exact(stream)?,
+                ))
+            }
+            <set_broken_route_error_code::Response as Parameter>::ID => {
+                Ok(Self::SetBrokenRouteErrorCode(
+                    set_broken_route_error_code::Response::from_le_stream_exact(stream)?,
+                ))
+            }
+            <set_child_data::Response as Parameter>::ID => Ok(Self::SetChildData(
+                set_child_data::Response::from_le_stream_exact(stream)?,
+            )),
+            <set_concentrator::Response as Parameter>::ID => Ok(Self::SetConcentrator(
+                set_concentrator::Response::from_le_stream_exact(stream)?,
+            )),
+            <set_duty_cycle_limits_in_stack::Response as Parameter>::ID => {
+                Ok(Self::SetDutyCycleLimitsInStack(
+                    set_duty_cycle_limits_in_stack::Response::from_le_stream_exact(stream)?,
+                ))
+            }
+            <set_logical_and_radio_channel::Response as Parameter>::ID => {
+                Ok(Self::SetLogicalAndRadioChannel(
+                    set_logical_and_radio_channel::Response::from_le_stream_exact(stream)?,
+                ))
+            }
+            <set_manufacturer_code::Response as Parameter>::ID => Ok(Self::SetManufacturerCode(
+                set_manufacturer_code::Response::from_le_stream_exact(stream)?,
+            )),
+            <set_neighbor_frame_counter::Response as Parameter>::ID => {
+                Ok(Self::SetNeighborFrameCounter(
+                    set_neighbor_frame_counter::Response::from_le_stream_exact(stream)?,
+                ))
+            }
+            <set_power_descriptor::Response as Parameter>::ID => Ok(Self::SetPowerDescriptor(
+                set_power_descriptor::Response::from_le_stream_exact(stream)?,
+            )),
+            <set_radio_channel::Response as Parameter>::ID => Ok(Self::SetRadioChannel(
+                set_radio_channel::Response::from_le_stream_exact(stream)?,
+            )),
+            <set_radio_ieee802154_cca_mode::Response as Parameter>::ID => {
+                Ok(Self::SetRadioIeee802154CcaMode(
+                    set_radio_ieee802154_cca_mode::Response::from_le_stream_exact(stream)?,
+                ))
+            }
+            <set_radio_power::Response as Parameter>::ID => Ok(Self::SetRadioPower(
+                set_radio_power::Response::from_le_stream_exact(stream)?,
+            )),
+            <set_routing_shortcut_threshold::Response as Parameter>::ID => {
+                Ok(Self::SetRoutingShortcutThreshold(
+                    set_routing_shortcut_threshold::Response::from_le_stream_exact(stream)?,
+                ))
+            }
+            <start_scan::Response as Parameter>::ID => Ok(Self::StartScan(
+                start_scan::Response::from_le_stream_exact(stream)?,
+            )),
+            <stop_scan::Response as Parameter>::ID => Ok(Self::StopScan(
+                stop_scan::Response::from_le_stream_exact(stream)?,
+            )),
+            <handler::ChildJoin as Parameter>::ID => Ok(Self::Handler(
+                handler::ChildJoin::from_le_stream_exact(stream)?.into(),
+            )),
+            <handler::DutyCycle as Parameter>::ID => Ok(Self::Handler(
+                handler::DutyCycle::from_le_stream_exact(stream)?.into(),
+            )),
+            <handler::EnergyScanResult as Parameter>::ID => Ok(Self::Handler(
+                handler::EnergyScanResult::from_le_stream_exact(stream)?.into(),
+            )),
+            <handler::NetworkFound as Parameter>::ID => Ok(Self::Handler(
+                handler::NetworkFound::from_le_stream_exact(stream)?.into(),
+            )),
+            <handler::ScanComplete as Parameter>::ID => Ok(Self::Handler(
+                handler::ScanComplete::from_le_stream_exact(stream)?.into(),
+            )),
+            <handler::StackStatus as Parameter>::ID => Ok(Self::Handler(
+                handler::StackStatus::from_le_stream_exact(stream)?.into(),
+            )),
+            <handler::UnusedPanIdFound as Parameter>::ID => Ok(Self::Handler(
+                handler::UnusedPanIdFound::from_le_stream_exact(stream)?.into(),
+            )),
+            _ => Err(Decode::InvalidFrameId(id)),
+        }
+    }
 }

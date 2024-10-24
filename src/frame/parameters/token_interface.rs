7a0e384b1631168eee5e660d3ab441119422993d
Richard Neumann <neumann@paulmann.de>
2024-10-24 09:13:06 +0200

    Remove command

--- src/frame/parameters/token_interface.rs
+++ src/frame/parameters/token_interface.rs
@@ -9,17 +9,6 @@ pub mod set_token_data;
 pub mod token_factory_reset;
 
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub enum Command {
-    GetTokenCount(get_token_count::Command),
-    GetTokenData(get_token_data::Command),
-    GetTokenInfo(get_token_info::Command),
-    GpSecurityTestVectors(gp_security_test_vectors::Command),
-    ResetNode(reset_node::Command),
-    SetTokenData(set_token_data::Command),
-    TokenFactoryReset(token_factory_reset::Command),
-}
-
-#[derive(Clone, Debug, Eq, PartialEq)]
 pub enum Response {
     GetTokenCount(get_token_count::Response),
     GetTokenData(get_token_data::Response),

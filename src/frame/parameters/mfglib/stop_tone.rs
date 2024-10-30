72f76f948c4fdf99e7ebe1b2a5241ee784dc1122
Richard Neumann <neumann@paulmann.de>
2024-10-30 14:42:29 +0100

    Add docs

--- src/frame/parameters/mfglib/stop_tone.rs
+++ src/frame/parameters/mfglib/stop_tone.rs
@@ -1,3 +1,5 @@
+//! Parameters for the [`Mfglib::stop_tone`](crate::Mfglib::stop_tone) command.
+
 use le_stream::derive::{FromLeStream, ToLeStream};
 use num_traits::FromPrimitive;
 
@@ -15,6 +17,7 @@ impl Identified for Command {
     const ID: Self::Id = ID;
 }
 
+/// Response parameters.
 #[derive(Clone, Debug, Eq, PartialEq, FromLeStream)]
 pub struct Response {
     status: u8,
@@ -25,6 +28,7 @@ impl Identified for Response {
     const ID: Self::Id = ID;
 }
 
+/// Converts the response into `()` or an appropriate [`Error`] depending on its status.
 impl TryFrom<Response> for () {
     type Error = Error;
 

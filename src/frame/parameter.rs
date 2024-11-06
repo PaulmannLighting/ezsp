1b751767ca5e7d5441a030ea45d057e5645d15e6
Richard Neumann <neumann@paulmann.de>
2024-11-06 09:19:00 +0100

    Add docs

--- src/frame/parameter.rs
+++ src/frame/parameter.rs
@@ -18,8 +18,19 @@ where
         + FromLeStream
         + ToLeStream,
 {
+    /// The type of the frame ID.
+    ///
+    /// This is usually `u16`, but may as well be `u8`.
     type Id;
+
+    /// The frame ID.
     const ID: Self::Id;
+
+    /// An optional disambiguation.
+    ///
+    /// This is necessary since there are multiple frames with the same ID.
     const DISAMBIGUATION: Option<Disambiguation> = None;
+
+    /// The unique ID of the frame consisting of the frame ID and the optional disambiguation.
     const UNIQUE_ID: (Self::Id, Option<Disambiguation>) = (Self::ID, Self::DISAMBIGUATION);
 }

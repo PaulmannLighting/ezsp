77679d5cf1e3decb27ee079fd1ae07aadf871e86
Richard Neumann <neumann@paulmann.de>
2025-11-28 14:30:14 +0100

    Make `aps::Options` bitflags.

--- src/ember/aps.rs
+++ src/ember/aps.rs
@@ -1,60 +1,57 @@
 //! Application Support Sublayer (APS) module.
 
+use bitflags::bitflags;
 use le_stream::derive::{FromLeStream, ToLeStream};
-use num_derive::FromPrimitive;
-use num_traits::FromPrimitive;
 
-/// A list of APS options.
-pub type Options = heapless::Vec<Option, 10>;
-
-/// Ember APS option.
-#[derive(Debug, Clone, Copy, Ord, PartialOrd, Eq, PartialEq, FromPrimitive)]
-#[repr(u16)]
-pub enum Option {
-    /// No options.
-    None = 0x0000,
-    /// Send the message using APS Encryption, using the Link Key shared with the
-    /// destination node to encrypt the data at the APS Level.
-    Encryption = 0x0020,
-    /// Resend the message using the APS retry mechanism.
-    Retry = 0x0040,
-    /// Causes a route discovery to be initiated if no route to the destination is known.
-    EnableRouteDiscovery = 0x0100,
-    /// Causes a route discovery to be initiated even if one is known.
-    ForceRouteDiscovery = 0x0200,
-    /// Include the source EUI64 in the network frame.
-    SourceEui64 = 0x0400,
-    /// Include the destination EUI64 in the network frame.
-    DestinationEui64 = 0x0800,
-    /// Send a ZDO request to discover the node ID of the destination, if it is not already know.
-    EnableAddressDiscovery = 0x1000,
-    /// Reserved.
-    PollResponse = 0x2000,
-    /// This incoming message is a ZDO request not handled by the `EmberZNet` stack,
-    /// and the application is responsible for sending a ZDO response.
-    ///
-    /// This flag is used only when the ZDO is configured to have requests handled by the application.
-    /// See the `EZSP_CONFIG_APPLICATION_ZDO_FLAGS` configuration parameter for more information.
-    ZdoResponseRequired = 0x4000,
-    /// This message is part of a fragmented message. This option may only be set for unicasts.
-    ///
-    /// The `groupId` field gives the index of this fragment in the low-order byte.
-    /// If the low-order byte is zero this is the first fragment and the high-order byte
-    /// contains the number of fragments in the message.
-    Fragment = 0x8000,
-}
-
-impl From<Option> for u16 {
-    fn from(option: Option) -> Self {
-        option as Self
+/// Ember APS options.
+#[derive(Debug, Clone, Copy, Ord, PartialOrd, Eq, PartialEq)]
+#[repr(transparent)]
+pub struct Options(u16);
+bitflags! {
+    impl Options: u16 {
+        /// No options.
+        const NONE = 0x0000;
+        /// Send the message using APS Encryption, using the Link Key shared with the
+        /// destination node to encrypt the data at the APS Level.
+        const ENCRYPTION = 0x0020;
+        /// Resend the message using the APS retry mechanism.
+        const RETRY = 0x0040;
+        /// Causes a route discovery to be initiated if no route to the destination is known.
+        const ENABLE_ROUTE_DISCOVERY = 0x0100;
+        /// Causes a route discovery to be initiated even if one is known.
+        const  FORCE_ROUTE_DISCOVERY = 0x0200;
+        /// Include the source EUI64 in the network frame.
+        const  SOURCE_EUI64 = 0x0400;
+        /// Include the destination EUI64 in the network frame.
+        const DESTINATION_EUI64 = 0x0800;
+        /// Send a ZDO request to discover the node ID of the destination, if it is not already know.
+        const ENABLE_ADDRESS_DISCOVERY = 0x1000;
+        /// Reserved.
+        const POLL_RESPONSE = 0x2000;
+        /// This incoming message is a ZDO request not handled by the `EmberZNet` stack,
+        /// and the application is responsible for sending a ZDO response.
+        ///
+        /// This flag is used only when the ZDO is configured to have requests handled by the application.
+        /// See the `EZSP_CONFIG_APPLICATION_ZDO_FLAGS` configuration parameter for more information.
+        const ZDO_RESPONSE_REQUIRED = 0x4000;
+        /// This message is part of a fragmented message. This option may only be set for unicasts.
+        ///
+        /// The `groupId` field gives the index of this fragment in the low-order byte.
+        /// If the low-order byte is zero this is the first fragment and the high-order byte
+        /// contains the number of fragments in the message.
+         const FRAGMENT = 0x8000;
     }
 }
 
-impl TryFrom<u16> for Option {
-    type Error = u16;
+impl From<Options> for u16 {
+    fn from(options: Options) -> Self {
+        options.0
+    }
+}
 
-    fn try_from(value: u16) -> Result<Self, Self::Error> {
-        Self::from_u16(value).ok_or(value)
+impl From<u16> for Options {
+    fn from(value: u16) -> Self {
+        Self::from_bits_truncate(value)
     }
 }
 
@@ -87,7 +84,7 @@ impl Frame {
             cluster_id,
             source_endpoint,
             destination_endpoint,
-            options: options.into_iter().map(u16::from).sum(),
+            options: options.0,
             group_id,
             sequence,
         }
@@ -123,37 +120,7 @@ impl Frame {
     /// This function will panic if the options buffer does not have sufficient capacity.
     #[must_use]
     pub fn options(&self) -> Options {
-        let mut options = Options::new();
-
-        if Option::try_from(self.options) == Ok(Option::None) {
-            return options;
-        }
-
-        if Option::Retry as u16 & self.options != 0 {
-            options
-                .push(Option::Retry)
-                .expect("Options buffer should have sufficient capacity. This is a bug.");
-        }
-
-        if Option::EnableRouteDiscovery as u16 & self.options != 0 {
-            options
-                .push(Option::EnableRouteDiscovery)
-                .expect("Options buffer should have sufficient capacity. This is a bug.");
-        }
-
-        if Option::ForceRouteDiscovery as u16 & self.options != 0 {
-            options
-                .push(Option::ForceRouteDiscovery)
-                .expect("Options buffer should have sufficient capacity. This is a bug.");
-        }
-
-        if Option::SourceEui64 as u16 & self.options != 0 {
-            options
-                .push(Option::SourceEui64)
-                .expect("Options buffer should have sufficient capacity. This is a bug.");
-        }
-
-        options
+        Options::from_bits_truncate(self.options)
     }
 
     /// Return the group ID for this message, if it is multicast mode.

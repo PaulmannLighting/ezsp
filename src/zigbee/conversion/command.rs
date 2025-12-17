fc46aad56ead5e8d617488c9268e109adfe8f1bb
Richard Neumann <neumann@paulmann.de>
2025-12-17 16:45:12 +0100

    Fix profile matching.

--- src/zigbee/conversion/command.rs
+++ src/zigbee/conversion/command.rs
@@ -13,14 +13,17 @@ impl TryFrom<DefragmentedMessage> for Command {
             Profile::try_from(aps_frame.profile_id()).map_err(ParseCommandError::InvalidProfile)?;
 
         match profile {
-            Profile::Unspecified => zdp::Frame::<zdp::Command>::try_from(frame)
+            Profile::Network => zdp::Frame::<zdp::Command>::try_from(frame)
                 .map(Command::Zdp)
                 .map_err(ParseCommandError::ParseZdpFrameError),
-            Profile::ZigbeeHomeAutomation | Profile::SmartEnergy | Profile::Touchlink => {
-                zcl::Frame::<zcl::Cluster>::try_from(frame)
-                    .map(Command::Zcl)
-                    .map_err(ParseCommandError::ParseZclFrameError)
-            }
+            Profile::ZigbeeHomeAutomation
+            | Profile::SmartEnergy
+            | Profile::TouchLink
+            | Profile::BuildingAutomation
+            | Profile::HealthCare
+            | Profile::RemoteControl => zcl::Frame::<zcl::Cluster>::try_from(frame)
+                .map(Command::Zcl)
+                .map_err(ParseCommandError::ParseZclFrameError),
         }
     }
 }

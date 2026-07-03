d80a3c5bcb94650c012cc0ef210a1a8e966a0c2c
Richard Neumann <neumann@paulmann.de>
2026-07-03 18:26:45 +0200

    Split macro.

--- src/frame/parameters.rs
+++ src/frame/parameters.rs
@@ -1,54 +1,64 @@
 //! Definitions of parameter structures used in the `Ember ZNet Serial Protocol` (`EZSP`).
 
+macro_rules! command {
+    ($id:expr, {}) => {
+        #[derive(Clone, Debug, Eq, PartialEq, le_stream::ToLeStream)]
+        pub(crate) struct Command;
+
+        impl crate::frame::Parameter for Command {
+            const ID: u16 = $id;
+        }
+    };
+    ($id:expr, { $($field:ident: $ty:ty),+ $(,)? }) => {
+        #[derive(Clone, Debug, Eq, PartialEq, le_stream::ToLeStream)]
+        pub(crate) struct Command {
+            $($field: $ty),+
+        }
+
+        impl crate::frame::Parameter for Command {
+            const ID: u16 = $id;
+        }
+    };
+}
+pub(crate) use command;
+
+macro_rules! response {
+    ($id:expr, {}) => {
+        /// Response parameters.
+        #[derive(Clone, Debug, Eq, PartialEq, le_stream::FromLeStream)]
+        pub struct Response;
+
+        impl crate::frame::Parameter for Response {
+            const ID: u16 = $id;
+        }
+    };
+    ($id:expr, { $($field:ident: $ty:ty),+ $(,)? }) => {
+        /// Response parameters.
+        #[derive(Clone, Debug, Eq, PartialEq, le_stream::FromLeStream)]
+        pub struct Response {
+            $($field: $ty),+
+        }
+
+        impl crate::frame::Parameter for Response {
+            const ID: u16 = $id;
+        }
+    };
+}
+pub(crate) use response;
+
 macro_rules! frame {
     (
         $id:expr,
         { $($command_field:ident: $command_ty:ty),* $(,)? },
         { $($response_field:ident: $response_ty:ty),* $(,)? }
     ) => {
-        const ID: u16 = $id;
-
-        frame! {
-            @command { $($command_field: $command_ty),* }
-        }
-
-        impl crate::frame::Parameter for Command {
-            const ID: u16 = ID;
-        }
+        crate::frame::parameters::command!($id, { $($command_field: $command_ty),* });
 
         impl crate::frame::RespondsWith for Command {
             type Response = Response;
         }
 
-        frame! {
-            @response { $($response_field: $response_ty),* }
-        }
-
-        impl crate::frame::Parameter for Response {
-            const ID: u16 = ID;
-        }
-    };
-    (@command {}) => {
-        #[derive(Clone, Debug, Eq, PartialEq, le_stream::ToLeStream)]
-        pub(crate) struct Command;
-    };
-    (@command { $($field:ident: $ty:ty),+ }) => {
-        #[derive(Clone, Debug, Eq, PartialEq, le_stream::ToLeStream)]
-        pub(crate) struct Command {
-            $($field: $ty),+
-        }
-    };
-    (@response {}) => {
-        /// Response parameters.
-        #[derive(Clone, Debug, Eq, PartialEq, le_stream::FromLeStream)]
-        pub struct Response;
-    };
-    (@response { $($field:ident: $ty:ty),+ }) => {
-        /// Response parameters.
-        #[derive(Clone, Debug, Eq, PartialEq, le_stream::FromLeStream)]
-        pub struct Response {
-            $($field: $ty),+
-        }
+        crate::frame::parameters::response!($id, { $($response_field: $response_ty),* });
     };
 }
 pub(crate) use frame;

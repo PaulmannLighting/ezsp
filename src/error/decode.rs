38da286250143ab344409deb42e3edd9474509ab
Richard Neumann <neumann@paulmann.de>
2026-07-19 14:39:41 +0200

    Use thiserror.

--- src/error/decode.rs
+++ src/error/decode.rs
@@ -1,13 +1,12 @@
-use core::fmt::Display;
-use std::error::Error;
-
 /// An error that occurs when decoding a frame.
-#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, thiserror::Error)]
 pub enum Decode {
     /// Too few bytes to decode.
+    #[error("Too few bytes to decode.")]
     TooFewBytes,
 
     /// Frame ID mismatch.
+    #[error("Frame ID mismatch: Expected {expected:#06X}, found {found:#06X}.")]
     FrameIdMismatch {
         /// The expected frame ID.
         expected: u16,
@@ -16,22 +15,6 @@ pub enum Decode {
     },
 
     /// Invalid frame ID.
+    #[error("Invalid frame ID: {0:#06X}.")]
     InvalidFrameId(u16),
 }
-
-impl Display for Decode {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::TooFewBytes => write!(f, "Too few bytes to decode."),
-            Self::FrameIdMismatch { expected, found } => {
-                write!(
-                    f,
-                    "Frame ID mismatch: Expected {expected:#06X}, found {found:#06X}."
-                )
-            }
-            Self::InvalidFrameId(id) => write!(f, "Invalid frame ID: {id:#06X}."),
-        }
-    }
-}
-
-impl Error for Decode {}
--- src/error/value_error.rs
+++ src/error/value_error.rs
@@ -1,62 +1,33 @@
-use core::fmt::Display;
 use core::num::TryFromIntError;
 
 /// Invalid values.
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq, thiserror::Error)]
 pub enum ValueError {
     /// An invalid frame ID for a legacy header was received.
-    InvalidFrameId(TryFromIntError),
+    #[error("Invalid frame ID for legacy frame format: {0}")]
+    InvalidFrameId(#[from] TryFromIntError),
 
     /// An invalid [`State`](crate::ember::duty_cycle::State) was received.
+    #[error("Invalid Ember duty cycle state: {0:#04X}")]
     EmberDutyCycleState(u8),
 
     /// An invalid [`Status`](crate::ember::network::Status) was received.
+    #[error("Invalid Ember network status: {0:#04X}")]
     EmberNetworkStatus(u8),
 
     /// An invalid [`Type`](crate::ember::node::Type) was received.
+    #[error("InvalidEmber node type: {0:#04X}")]
     EmberNodeType(u8),
 
     /// The decision ID is invalid.
+    #[error("Invalid decision ID: {0:#04X}")]
     DecisionId(u8),
 
     /// The decision ID is invalid.
+    #[error("Invalid entropy source: {0:#04X}")]
     EntropySource(u8),
 
     /// Indicates that some expected payload was missing.
+    #[error("Missing payload")]
     MissingPayload,
-}
-
-impl Display for ValueError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::InvalidFrameId(error) => {
-                write!(f, "Invalid frame ID for legacy frame format: {error}")
-            }
-            Self::EmberDutyCycleState(state) => {
-                write!(f, "Invalid Ember duty cycle state: {state:#04X}")
-            }
-            Self::EmberNetworkStatus(status) => {
-                write!(f, "Invalid Ember network status: {status:#04X}")
-            }
-            Self::EmberNodeType(node_type) => write!(f, "InvalidEmber node type: {node_type:#04X}"),
-            Self::DecisionId(id) => write!(f, "Invalid decision ID: {id:#04X}"),
-            Self::EntropySource(source) => write!(f, "Invalid entropy source: {source:#04X}"),
-            Self::MissingPayload => write!(f, "Missing payload"),
-        }
-    }
-}
-
-impl std::error::Error for ValueError {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        match self {
-            Self::InvalidFrameId(error) => Some(error),
-            _ => None,
-        }
-    }
-}
-
-impl From<TryFromIntError> for ValueError {
-    fn from(error: TryFromIntError) -> Self {
-        Self::InvalidFrameId(error)
-    }
 }
